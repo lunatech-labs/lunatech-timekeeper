775caea2890fbd10ad7b29997d7c09491293ae61
Nicolas Martignole <nicolas@touilleur-express.fr>
2020-04-05 21:00:38 +0200

    inutile

--- src/main/java/fr/lunatech/timekeeper/UserResource.java
+++ src/main/java/fr/lunatech/timekeeper/UserResource.java
@@ -28,14 +28,4 @@ public class UserResource {
         return new User(identity);
     }
 
-    @GET
-    @Path("/decode")
-    @Produces(MediaType.APPLICATION_JSON)
-    @NoCache
-    public Map decode() {
-        Map map = new HashMap();
-        map.put("token" , new User(identity));
-        map.put("expired", false);
-        return map;
-    }
 }
