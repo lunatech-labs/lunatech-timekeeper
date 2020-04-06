ff755311713647a0d3a6dd4a8c6690e5a601d457
Fabrice Sznajderman <fabszn@protonmail.com>
2020-04-06 16:49:39 +0200

    add logging dependency + configuration (#6)

--- src/main/java/fr/lunatech/timekeeper/ExampleResource.java
+++ src/main/java/fr/lunatech/timekeeper/ExampleResource.java
@@ -4,6 +4,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/hello")
 public class ExampleResource {
@@ -11,6 +13,9 @@ public class ExampleResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        Logger logger = LoggerFactory.getLogger(ExampleResource.class);
+        logger.info("Hello World");
+
         return "hello";
     }
 }
