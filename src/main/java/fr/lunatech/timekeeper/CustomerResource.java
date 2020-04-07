42936da784c8354b379762b529450af5901e480f
Fabrice Sznajderman <fabszn@protonmail.com>
2020-04-07 09:44:02 +0200

    Feature/tk 76 add h2 for tu (#10)
    * fix: set secret in the JSON file for Keycloak (#9)
    
    * Google API : set secret for Dev
    
    * Fix secret for Docker and Keycloak environment
    
    Set the secret value to some string
    
    second
    
    * add H2 as memoryDB for Test
    
    * disabling keycloak during run of Unit Tests
    
    Co-authored-by: Nicolas Martignole <nicolas.martignole@lunatech.com>

---
+++ src/main/java/fr/lunatech/timekeeper/CustomerResource.java
@@ -0,0 +1,32 @@
+package fr.lunatech.timekeeper;
+
+import fr.lunatech.timekeeper.model.Customer;
+import fr.lunatech.timekeeper.services.CustomerService;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.Optional;
+
+@Path("/customers")
+public class CustomerResource {
+
+    @Inject
+    CustomerService customerService;
+
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public long newCustomer(Customer customer) {
+        return customerService.addCustomer(customer);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public Optional<Customer> readCustomerById(@PathParam("id") long id) {
+        return customerService.getCustomerById(id);
+    }
+
+
+}
