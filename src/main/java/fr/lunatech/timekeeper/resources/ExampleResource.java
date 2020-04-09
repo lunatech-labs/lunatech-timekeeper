3f0909469d3188786ef3bc470df13f9fa90268f7
Fabrice Sznajderman <fabszn@protonmail.com>
2020-04-09 10:17:32 +0200

    re add ExempleResource class

---
+++ src/main/java/fr/lunatech/timekeeper/resources/ExampleResource.java
@@ -0,0 +1,21 @@
+package fr.lunatech.timekeeper.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Path("/hello")
+public class ExampleResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        Logger logger = LoggerFactory.getLogger(ExampleResource.class);
+        logger.info("Hello World");
+
+        return "hello";
+    }
+}
