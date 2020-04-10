4d40ba6b6dafa131659d1750a978d8dd34a66ace
Coline <coline.thomas@lunatech.fr>
2020-04-10 10:34:35 +0200

    add page new customer

--- frontend/src/components/pages/Customer/NewCustomer.js
+++ frontend/src/components/pages/Customer/NewCustomer.js
@@ -1,8 +1,25 @@
 import React from 'react'
-import { Button } from 'antd'
+import {Button, Form, Input, Select } from 'antd'
 
 const NewCustomer = ({ }) => (
-    <Button className="btn new"> New </Button>
+    <Form
+        labelCol={{span: 4}}
+        wrapperCol={{span: 14}}
+        layout="horizontal"
+    >
+        <Form.Item label="Name">
+            <Input placeholder="New customer name"/>
+        </Form.Item>
+        <Form.Item label="Project">
+            <Select>
+                <Select.Option value="demo">DARVA - Agira</Select.Option>
+                <Select.Option value="demo">DARVA - Sinapps</Select.Option>
+                <Select.Option value="demo">Disney - Guest profile</Select.Option>
+            </Select>
+        </Form.Item>
+        <Button className="btn save">Save</Button>
+        <Button className="btn cancel">Cancel</Button>
+    </Form>
 );
 
 export default NewCustomer;
