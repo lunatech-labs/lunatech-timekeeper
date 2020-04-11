ce30ae3b1222d165203cccc6d895ac0bb3d8d6c6
SFE <30528315+sfelix77@users.noreply.github.com>
2020-04-11 19:38:55 +0200

    test for delete member to activity

--- src/test/java/fr/lunatech/timekeeper/resources/MemberResourceTest.java
+++ src/test/java/fr/lunatech/timekeeper/resources/MemberResourceTest.java
@@ -29,7 +29,7 @@ class MemberResourceTest {
     }
 
     @Test
-    public void testPostMemberResourcesEndpoint() {
+    public void testAddMemberToActivityResourcesEndpoint() {
 
         given()
                 .when().contentType(MediaType.APPLICATION_JSON).body("{\"firstName\":\"Sam\",\"lastName\":\"Huel\",\"email\":\"sam@gmail.com\", \"profiles\":[\"Admin\"]}").post("/api/users")
@@ -60,7 +60,7 @@ class MemberResourceTest {
     }
 
     @Test
-    public void testPostMemberResourcesWithWrongUserIdEndpoint() {
+    public void testAddMemberToActivityResourcesWithWrongUserIdEndpoint() {
         given()
                 .when().contentType(MediaType.APPLICATION_JSON).body("{\"role\":\"Developer\", \"userId\":12}").post("/api/members")
                 .then()
@@ -99,12 +99,6 @@ class MemberResourceTest {
                 .statusCode(200).body(is("4"));
 
         given()
-                .when().get("/api/activities/3/members/4")
-                .then()
-                .statusCode(200)
-                .body(is("{\"role\":\"Developer\",\"userId\":1,\"id\":4}"));
-
-        given()
                 .when().contentType(MediaType.APPLICATION_JSON).body("\"TeamLeader\"").put("/api/activities/3/members/4")
                 .then()
                 .statusCode(200).body(is("4"));
@@ -114,5 +108,40 @@ class MemberResourceTest {
                 .then()
                 .statusCode(200)
                 .body(is("{\"role\":\"TeamLeader\",\"userId\":1,\"id\":4}"));
+    }
+
+    @Test
+    public void testDeleteMemberToActivityResourcesEndpoint() {
+
+        given()
+                .when().contentType(MediaType.APPLICATION_JSON).body("{\"firstName\":\"Sam\",\"lastName\":\"Huel\",\"email\":\"sam@gmail.com\", \"profiles\":[\"Admin\"]}").post("/api/users")
+                .then()
+                .statusCode(200).body(is("1"));
+
+        given()
+                .when().contentType(MediaType.APPLICATION_JSON).body("{\"name\":\"NewClient\"}").post("/api/customers")
+                .then()
+                .statusCode(200).body(is("2"));
+
+        given()
+                .when().contentType(MediaType.APPLICATION_JSON).body("{\"name\":\"Pepito\",\"billable\":true,\"description\":\"New project\", \"customerId\":2, \"members\":[]}").post("/api/activities")
+                .then()
+                .statusCode(200).body(is("3"));
+
+        given()
+                .when().contentType(MediaType.APPLICATION_JSON).body("{\"role\":\"Developer\", \"userId\":1}").post("/api/activities/3/members")
+                .then()
+                .statusCode(200).body(is("4"));
+
+        given()
+                .when().contentType(MediaType.APPLICATION_JSON).delete("/api/activities/3/members/4")
+                .then()
+                .statusCode(200)
+                .body(is("4"));
+
+        given()
+                .when().get("/api/activities/3/members/4")
+                .then()
+                .statusCode(404);
     }
 }
