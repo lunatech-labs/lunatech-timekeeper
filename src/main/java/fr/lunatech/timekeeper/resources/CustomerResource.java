0b5009bd82ed95225545fc1ea8fe668fde1baf90
SFE <30528315+sfelix77@users.noreply.github.com>
2020-04-14 15:34:12 +0200

    fix flyway bugs

--- src/main/java/fr/lunatech/timekeeper/resources/CustomerResource.java
+++ src/main/java/fr/lunatech/timekeeper/resources/CustomerResource.java
@@ -11,35 +11,23 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/api/customers")
 public class CustomerResource implements CustomerResourceApi {
 
     @Inject
     CustomerService customerService;
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<CustomerResponse> readAllCustomers() {
         return customerService.getAllCustomers();
     }
 
-
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
     public Response newCustomer(CustomerRequest request) {
         return Response.ok(customerService.addCustomer(request)).build();
     }
 
-    @GET
-    @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public CustomerResponse readCustomerById(@PathParam("id") long id) {
         return customerService.getCustomerById(id).orElseThrow(NotFoundException::new);
     }
 
-    @PUT
-    @Path("/{id}")
-    @Consumes(MediaType.APPLICATION_JSON)
     public Response updateCustomer(@PathParam("id") long id, CustomerRequest request){
         return Response.ok(customerService.updateCustomer(id, request).orElseThrow(NotFoundException::new)).build();
     }
