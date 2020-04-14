bf9708d35fc54ba5839697968312d26a46142b9a
SFE <30528315+sfelix77@users.noreply.github.com>
2020-04-14 19:36:50 +0200

    add getAllActivities endpoint + all tests + move openapi under /api

--- src/main/java/fr/lunatech/timekeeper/services/ActivityService.java
+++ src/main/java/fr/lunatech/timekeeper/services/ActivityService.java
@@ -3,9 +3,11 @@ package fr.lunatech.timekeeper.services;
 import fr.lunatech.timekeeper.dtos.ActivityRequest;
 import fr.lunatech.timekeeper.dtos.ActivityResponse;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ActivityService {
+    List<ActivityResponse> listAllActivities();
     Long createActivity(ActivityRequest request);
     Optional<ActivityResponse> findActivityById(Long id);
     Optional<Long> updateActivity(Long id, ActivityRequest activity);
