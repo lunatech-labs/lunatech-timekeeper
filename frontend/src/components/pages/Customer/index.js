df273e57f142266fbfe71ffea82b26bb0c021688
gdavy <guillaume.davy@lunatech.fr>
2020-04-15 15:25:35 +0200

    axiosInstance in index.js and given to the component

--- frontend/src/components/pages/Customer/index.js
+++ frontend/src/components/pages/Customer/index.js
@@ -5,11 +5,13 @@ import logo from '../../../logo_timekeeper_homepage.png';
 import CustomerDetails from "./CustomerDetails";
 import MainContainer from "../../container/MainContainer";
 import NewCustomer from "./NewCustomer";
+import {useAxios} from "../../../utils/hooks";
 
 const list = [{"id": 1, "name": "Paul"}, {"id": 2, "name": "Nicolas"},
     {"id":3, "name": "Marie"}, {"id": 4, "name": "Stephan"}, {"id": 5, "name": "Camille"}];
 
 const CustomersPage = ({ }) => {
+    const axiosInstance = useAxios('http://localhost:8080');
     const { pathname } = useLocation();
     const selectedCustomer = pathname.split('/').length > 2
         ? pathname.split('/')[2] === 'new'
@@ -22,7 +24,7 @@ const CustomersPage = ({ }) => {
             ? selectedCustomer === 'new'
                 ? (
                     <MainContainer title="Add new customer">
-                        <NewCustomer list={list} logo={logo}/>
+                        <NewCustomer list={list} logo={logo} axiosInstance={axiosInstance}/>
                     </MainContainer>
                 )
                 : (
