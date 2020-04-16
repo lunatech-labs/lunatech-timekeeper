343faf6dc81347a185aa8c36a4feff49a175dfa2
Nicolas Martignole <nicolas.martignole@lunatech.com>
2020-04-16 19:13:38 +0200

    React: reorganize various files and clean-up (#46)
    * Migration to yarn and to custome react-app-rewired
    
    Let us customize the Theme
    
    * WIP with Nicolas - setup of Less and the main colors
    
    * clean-up
    
    * Highlight menu section depending on the browser route
    
    * Refactor and rearrange less resources
    
    * Reorganize the components and the pages in different package
    
    Co-authored-by: geoffroy bouet <geoffroy.bouet@lunatech.fr>

--- frontend/src/components/MenuSidebar/MenuSidebar.js
+++ frontend/src/components/MenuSidebar/MenuSidebar.js
@@ -1,67 +1,31 @@
-import React, { Component } from 'react';
-import {Link} from "react-router-dom";
+import React, {Component} from 'react';
+import {Link, withRouter} from "react-router-dom";
 import {Menu} from "antd";
-import './MenuSidebar.scss';
+import './MenuSidebar.less';
+import PropTypes from "prop-types";
 
-import {DesktopOutlined, PieChartOutlined, TeamOutlined, UserOutlined} from "@ant-design/icons";
-import LinkButton from "../atoms/LinkButton";
-
-const { SubMenu } = Menu;
+import {DesktopOutlined, PieChartOutlined} from "@ant-design/icons";
 
 class MenuSidebar extends Component {
+    static propTypes = {
+        location: PropTypes.object.isRequired,
+    };
+
     render() {
+        const {location} = this.props;
         return (
-            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" className="tk_Menu">
-                <Menu.Item key="1">
-                    <PieChartOutlined />
-                    <Link
-                        to="/home"
-                        className="sider-link"
-                        type="link"
-                        ghost
-                    >
-                        Home
-                    </Link>
+            <Menu defaultSelectedKeys={['/home']} mode="inline" selectedKeys={[location.pathname]}  theme="dark">
+                <Menu.Item key="/home">
+                    <PieChartOutlined/>
+                    <Link to="/home">Home</Link>
                 </Menu.Item>
-                <Menu.Item key="2">
-                    <DesktopOutlined />
-                    <LinkButton
-                        to="/customers"
-                        className="sider-link"
-                        type="link"
-                        ghost
-                    >
-                        Customers
-                    </LinkButton>
+                <Menu.Item key="/customers">
+                    <DesktopOutlined/>
+                    <Link to="/customers">Customers</Link>
                 </Menu.Item>
-                <SubMenu
-                    key="sub1"
-                    title={
-                        <span>
-                            <UserOutlined />
-                            <span>User</span>
-                        </span>
-                    }
-                >
-                    <Menu.Item key="3">Tom</Menu.Item>
-                    <Menu.Item key="4">Bill</Menu.Item>
-                    <Menu.Item key="5">Alex</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                    key="sub2"
-                    title={
-                        <span>
-                            <TeamOutlined />
-                            <span>Team</span>
-                        </span>
-                    }
-                >
-                    <Menu.Item key="6">Team 1</Menu.Item>
-                    <Menu.Item key="8">Team 2</Menu.Item>
-                </SubMenu>
             </Menu>
         )
     }
 }
 
-export default MenuSidebar;
+export default withRouter(MenuSidebar);
