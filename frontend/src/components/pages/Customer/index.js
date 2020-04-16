0508668c7b1b299fe2f054e66ebbbd1044bdd0d7
thierry kuy <kuy.thierry@gmail.com>
2020-04-16 10:07:56 +0200

    Remove double ternary. Enhanced readability on CustomerList. Remove hard coded data.

--- frontend/src/components/pages/Customer/index.js
+++ frontend/src/components/pages/Customer/index.js
@@ -7,10 +7,6 @@ import MainContainer from "../../container/MainContainer";
 import NewCustomer from "./NewCustomer";
 import {useAxios} from "../../../utils/hooks";
 
-const list = [{"id": 1, "name": "Paul"}, {"id": 2, "name": "Nicolas"},
-    {"id":3, "name": "Marie"}, {"id": 4, "name": "Stephan"}, {"id": 5, "name": "Camille"}];
-
-
 const getCustomerList = (axios, setState) => {
     const fetchData = async () => {
         const result = await axios.get('/api/customers');
@@ -26,16 +22,25 @@ const getActivityList = (axios, setState) => {
     fetchData();
 };
 
+const selectCustomer = (pathname, customers) => {
+    const splitPathname = pathname.split('/');
+    if(splitPathname.length > 2) {
+        if(splitPathname[2] === 'new') {
+            return 'new';
+        } else {
+            return customers.find(c => c.id = splitPathname[2]);
+        }
+    } else {
+        return null;
+    }
+};
+
 const CustomersPage = ({ }) => {
     const [customers, setCustomers] = useState([]);
     const [activities, setActivities] = useState([]);
     const apiEndpoint = useAxios('http://localhost:8080');
     const { pathname } = useLocation();
-    const selectedCustomer = pathname.split('/').length > 2
-        ? pathname.split('/')[2] === 'new'
-            ? 'new'
-            : list.find(c => c.id = pathname.split('/')[2])
-        : null;
+    const selectedCustomer = selectCustomer(pathname, customers);
 
     useEffect(() => {
         if(!apiEndpoint) {
@@ -43,12 +48,8 @@ const CustomersPage = ({ }) => {
         }
 
         if(!selectedCustomer) {
-            getCustomerList(apiEndpoint,l => setCustomers(l));
-            getActivityList(apiEndpoint,l => setActivities(l));
-        }
-        return () => {
-            setCustomers([]);
-            setActivities([]);
+            getCustomerList(apiEndpoint,customers => setCustomers(customers));
+            getActivityList(apiEndpoint,activities => setActivities(activities));
         }
     }, [apiEndpoint, selectedCustomer]);
 
@@ -57,7 +58,7 @@ const CustomersPage = ({ }) => {
             ? selectedCustomer === 'new'
                 ? (
                     <MainContainer title="Add new customer">
-                        <NewCustomer list={list} logo={logo}/>
+                        <NewCustomer list={customers} logo={logo}/>
                     </MainContainer>
                 )
                 : (
@@ -67,7 +68,7 @@ const CustomersPage = ({ }) => {
                 )
             : (
                 <MainContainer title="All customers">
-                    <CustomerList list={customers} logo={logo} activities={activities}/>
+                    <CustomerList customers={customers} logo={logo} activities={activities}/>
                 </MainContainer>
             )
     )
