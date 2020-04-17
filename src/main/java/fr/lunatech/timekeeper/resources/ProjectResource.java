d988ba18ed1e392becd1ff302ad64b10a3b844da
SFE <30528315+sfelix77@users.noreply.github.com>
2020-04-17 19:49:34 +0200

    some fix about CamelCase

--- src/main/java/fr/lunatech/timekeeper/resources/ProjectResource.java
+++ src/main/java/fr/lunatech/timekeeper/resources/ProjectResource.java
@@ -26,19 +26,19 @@ public class ProjectResource implements ProjectResourceApi {
     }
 
     @Override
-    public Response createproject(@Valid ProjectRequest request, UriInfo uriInfo) {
+    public Response createProject(@Valid ProjectRequest request, UriInfo uriInfo) {
         final long projectId = projectService.createProject(request);
         final URI uri = uriInfo.getAbsolutePathBuilder().path(Long.toString(projectId)).build();
         return Response.created(uri).build();
     }
 
     @Override
-    public ProjectResponse getproject(Long id) {
+    public ProjectResponse getProject(Long id) {
         return projectService.findProjectById(id).orElseThrow(NotFoundException::new);
     }
 
     @Override
-    public Response updateproject(Long id, @Valid ProjectRequest request) {
+    public Response updateProject(Long id, @Valid ProjectRequest request) {
         projectService.updateProject(id, request).orElseThrow(NotFoundException::new);
         return Response.noContent().build();
     }
