6e9144693707282ac571927809a0675f9389f8d3
Nicolas Martignole <nicolas@touilleur-express.fr>
2020-04-21 16:30:52 +0200

    Logs to check if React calls the backend

--- src/main/java/fr/lunatech/timekeeper/resources/ClientResource.java
+++ src/main/java/fr/lunatech/timekeeper/resources/ClientResource.java
@@ -1,9 +1,12 @@
 package fr.lunatech.timekeeper.resources;
 
+import fr.lunatech.timekeeper.services.MemberServiceImpl;
 import fr.lunatech.timekeeper.services.dtos.ClientRequest;
 import fr.lunatech.timekeeper.services.dtos.ClientResponse;
 import fr.lunatech.timekeeper.resources.openapi.ClientResourceApi;
 import fr.lunatech.timekeeper.services.interfaces.ClientService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
@@ -15,6 +18,8 @@ import java.util.List;
 
 public class ClientResource implements ClientResourceApi {
 
+    private static Logger logger = LoggerFactory.getLogger(MemberServiceImpl.class);
+
     @Inject
     ClientService clientService;
 
@@ -25,7 +30,9 @@ public class ClientResource implements ClientResourceApi {
 
     @Override
     public Response createClient(@Valid ClientRequest request, UriInfo uriInfo) {
+        logger.debug("Create a new client with request="+request);
         final long clientId = clientService.createClient(request);
+        logger.debug("Created client with clientId="+clientId);
         final URI uri = uriInfo.getAbsolutePathBuilder().path(Long.toString(clientId)).build();
         return Response.created(uri).build();
     }
