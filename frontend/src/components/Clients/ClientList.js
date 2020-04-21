7c1ad9f93e18d55e1512155350f5a0c65ee03952
Nicolas Martignole <nicolas@touilleur-express.fr>
2020-04-21 17:10:46 +0200

    Removed useAxios and created 2 new services

--- frontend/src/components/Clients/ClientList.js
+++ frontend/src/components/Clients/ClientList.js
@@ -1,98 +1,85 @@
-import React, {useEffect, useState} from 'react';
-import PropTypes from 'prop-types';
-import {Avatar, Button, List, PageHeader} from 'antd';
+import React from 'react';
+import {Alert, Avatar, Button, List, PageHeader, Spin} from 'antd';
 import {EditOutlined, MoreOutlined} from '@ant-design/icons';
 import {Link} from 'react-router-dom';
-import {useAxios} from '../../utils/hooks';
 import logo from '../../img/logo_timekeeper_homepage.png';
-
-const getClientList = (axios, setState) => {
-  const fetchData = async () => {
-    const result = await axios.get('/api/clients');
-    setState(result.data);
-  };
-  fetchData();
-};
-const getProjectList = (axios, setState) => {
-  const fetchData = async () => {
-    const result = await axios.get('/api/projects');
-    setState(result.data);
-  };
-  fetchData();
-};
+import {useTimeKeeperAPI} from '../../utils/services';
 
 const ClientList = () => {
 
-  const [clients, setClients] = useState([]);
-  const [projects, setProjects] = useState([]);
-  const apiEndpoint = useAxios('http://localhost:8080');
-
-  useEffect(() => {
-    if (!apiEndpoint) {
-      return;
-    }
-    getClientList(apiEndpoint, setClients);
-    getProjectList(apiEndpoint, setProjects);
-  }, [apiEndpoint]);
-
+  const clientsResponse = useTimeKeeperAPI('/api/clients');
+  const projectsResponse = useTimeKeeperAPI('/api/projects');
 
   const projectsIdToProjects = (projectsId) => {
-    return projects.filter(project => projectsId.includes(project.id));
+    if (projectsResponse.data) {
+      return projectsResponse.data.filter(
+          project => projectsId.includes(project.id));
+    }
+    return [];
   };
 
-  const renderProjects = (projectsId) => projectsIdToProjects(projectsId).map(project => project.name).join(' | ');
+  const renderProjects = (projectsId) => projectsIdToProjects(projectsId).
+      map(project => project.name).
+      join(' | ');
+
+  if (clientsResponse.loading) {
+    return (
+        <React.Fragment>
+          <Spin size="large">
+            <p>Loading client</p>
+          </Spin>
+        </React.Fragment>
+
+    );
+  }
+  if (clientsResponse.error) {
+    return (
+        <React.Fragment>
+          <Alert title='Server error'
+                 message='Failed to load the list of clients'
+                 type='error'
+                 description={clientsResponse.error}
+          />
+        </React.Fragment>
+    );
+  }
 
   return (
-    <React.Fragment>
-      <PageHeader title="Clients" subTitle={clients.length} />
-      <List
-        itemLayout="horizontal"
-        dataSource={clients}
-        renderItem={item => (
-          <List.Item key={item.id}
-            actions={[
-              <Link key="editLink" to={`/clients/${item.id}`}>
-                <Button type="default" icon={<EditOutlined/>}>Edit</Button>
-              </Link>,
-              <Link key="moreLink" to={`/clients/${item.id}`}>
-                <Button type="default" icon={<MoreOutlined />}/>
-              </Link>
-            ]}
-          >
-            <List.Item.Meta
-              avatar={
-                <Avatar src={logo}/>
-              }
-              title={item.name}
-              description={item.description}
-            />
-            <div>{renderProjects(item.projectsId)}</div>
-          </List.Item>
-        )}
-      />
-      <Link to="/clients/new">
-        <Button type="primary">Create new client</Button>
-      </Link>
+      <React.Fragment>
+        <PageHeader title="Clients" subTitle={clientsResponse.data.length}/>
+        <List
+            itemLayout="horizontal"
+            dataSource={clientsResponse.data}
+            renderItem={item => (
+                <List.Item key={item.id}
+                           actions={[
+                             <Link key="editLink" to={`/clients/${item.id}`}>
+                               <Button type="default"
+                                       icon={<EditOutlined/>}>Edit</Button>
+                             </Link>,
+                             <Link key="moreLink" to={`/clients/${item.id}`}>
+                               <Button type="default" icon={<MoreOutlined/>}/>
+                             </Link>,
+                           ]}
+                >
+                  <List.Item.Meta
+                      avatar={
+                        <Avatar src={logo}/>
+                      }
+                      title={item.name}
+                      description={item.description}
+                  />
+                  <div>{renderProjects(item.projectsId)}</div>
+                </List.Item>
+            )}
+        />
+        <Link to="/clients/new">
+          <Button type="primary">Create new client</Button>
+        </Link>
 
 
-
-    </React.Fragment>
+      </React.Fragment>
   );
-};
-
-ClientList.propTypes = {
-  clients: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number,
-      name: PropTypes.string,
-      projectsId: PropTypes.arrayOf(PropTypes.number)
-    })
-  ),
-  logo: PropTypes.string,
-  projects: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number,
-    name: PropTypes.string
-  }))
 };
 
 export default ClientList;
