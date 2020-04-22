fc3422d54255494ab82ff0edacfa098036c2d007
Nicolas Martignole <nicolas@touilleur-express.fr>
2020-04-22 11:50:16 +0200

    Remove roles from the endpoint for the time being as it is not requiered in this PR

--- src/main/java/fr/lunatech/timekeeper/resources/ClientResource.java
+++ src/main/java/fr/lunatech/timekeeper/resources/ClientResource.java
@@ -8,7 +8,6 @@ import fr.lunatech.timekeeper.services.interfaces.ClientService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.security.PermitAll;
 import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.ws.rs.NotFoundException;
@@ -16,8 +15,6 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
 import java.util.List;
-
-import javax.annotation.security.RolesAllowed;
 
 public class ClientResource implements ClientResourceApi {
 
@@ -27,13 +24,11 @@ public class ClientResource implements ClientResourceApi {
     ClientService clientService;
 
     @Override
-    @PermitAll
     public List<ClientResponse> getAllClients() {
         return clientService.listAllClients();
     }
 
     @Override
-    @RolesAllowed("user")
     public Response createClient(@Valid ClientRequest request, UriInfo uriInfo) {
         logger.debug("Create a new client with request="+request);
         final long clientId = clientService.createClient(request);
@@ -43,13 +38,11 @@ public class ClientResource implements ClientResourceApi {
     }
 
     @Override
-    @PermitAll
     public ClientResponse getClient(Long id) {
         return clientService.findClientById(id).orElseThrow(NotFoundException::new);
     }
 
     @Override
-    @RolesAllowed("user")
     public Response updateClient(Long id, @Valid ClientRequest request) {
         clientService.updateClient(id, request).orElseThrow(NotFoundException::new);
         return Response.noContent().build();
