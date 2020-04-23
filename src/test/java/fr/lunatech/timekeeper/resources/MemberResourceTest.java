8ed5bb56dd6223356507f246487b36b5f74ac919
SFE <30528315+sfelix77@users.noreply.github.com>
2020-04-23 20:45:30 +0200

    fix Resource Conflict problems, an other PR is required to fix that fix some tests

--- src/test/java/fr/lunatech/timekeeper/resources/MemberResourceTest.java
+++ src/test/java/fr/lunatech/timekeeper/resources/MemberResourceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import javax.inject.Inject;
 
 import static fr.lunatech.timekeeper.models.Profile.Admin;
+import static fr.lunatech.timekeeper.models.Profile.User;
 import static fr.lunatech.timekeeper.resources.TestUtils.*;
 import static io.restassured.RestAssured.given;
 import static javax.ws.rs.core.HttpHeaders.ACCEPT;
@@ -154,7 +155,7 @@ class MemberResourceTest {
                 .get("/api/projects/3/members")
                 .then()
                 .statusCode(OK.getStatusCode())
-                .body(is(TestUtils.<MemberResponse>listOfTasJson(expectedMember1, expectedMember2)));
+                .body(is(TestUtils.listOfTasJson(expectedMember1, expectedMember2)));
     }
 
 
@@ -204,21 +205,67 @@ class MemberResourceTest {
 
     @Test
     void shouldNotAddMemberToprojectWithUnknownUser() {
+
+        final ClientRequest client = new ClientRequest("NewClient", "NewDescription");
+
+        given()
+                .when()
+                .contentType(APPLICATION_JSON)
+                .body(client)
+                .post("/api/clients")
+                .then()
+                .statusCode(CREATED.getStatusCode())
+                .header(LOCATION, endsWith("/api/clients/1"));
+
+        final ProjectRequest project = new ProjectRequest("Pepito", true, "New project", 1L);
+
+        given()
+                .when()
+                .contentType(APPLICATION_JSON)
+                .body(project)
+                .post("/api/projects")
+                .then()
+                .statusCode(CREATED.getStatusCode())
+                .header(LOCATION, endsWith("/api/projects/2"));
+
         given()
                 .when()
                 .contentType(APPLICATION_JSON)
                 .body("{\"role\":\"Developer\", \"userId\":12}")
-                .post("/api/members")
+                .post("/api/projects/2/members")
                 .then()
-                .statusCode(NOT_FOUND.getStatusCode());
+                .statusCode(BAD_REQUEST.getStatusCode());
     }
 
     @Test
     void shouldNotFindUnknownMember() {
+
+        final ClientRequest client = new ClientRequest("NewClient", "NewDescription");
+
+        given()
+                .when()
+                .contentType(APPLICATION_JSON)
+                .body(client)
+                .post("/api/clients")
+                .then()
+                .statusCode(CREATED.getStatusCode())
+                .header(LOCATION, endsWith("/api/clients/1"));
+
+        final ProjectRequest project = new ProjectRequest("Pepito", true, "New project", 1L);
+
+        given()
+                .when()
+                .contentType(APPLICATION_JSON)
+                .body(project)
+                .post("/api/projects")
+                .then()
+                .statusCode(CREATED.getStatusCode())
+                .header(LOCATION, endsWith("/api/projects/2"));
+
         given()
                 .when()
                 .header(ACCEPT, APPLICATION_JSON)
-                .get("/api/members/4")
+                .get("/api/projects/2/members/12")
                 .then()
                 .statusCode(NOT_FOUND.getStatusCode());
     }
@@ -353,5 +400,79 @@ class MemberResourceTest {
                 .delete("/api/projects/3/members/4")
                 .then()
                 .statusCode(NO_CONTENT.getStatusCode());
+    }
+
+    @Test
+    void shouldGetAllMembers() {
+
+        final UserRequest user = createUserRequest("Sam", "Huel", "sam@gmail.com", Admin);
+        given()
+                .when()
+                .contentType(APPLICATION_JSON)
+                .body(user)
+                .post("/api/users")
+                .then()
+                .statusCode(CREATED.getStatusCode())
+                .header(LOCATION, endsWith("/api/users/1"));
+
+        final UserRequest user2 = createUserRequest("Jimmy", "Pastore", "jimmy@gmail.com", User);
+        given()
+                .when()
+                .contentType(APPLICATION_JSON)
+                .body(user2)
+                .post("/api/users")
+                .then()
+                .statusCode(CREATED.getStatusCode())
+                .header(LOCATION, endsWith("/api/users/2"));
+
+        final ClientRequest client = new ClientRequest("NewClient", "NewDescription");
+        given()
+                .when()
+                .contentType(APPLICATION_JSON)
+                .body(client)
+                .post("/api/clients")
+                .then()
+                .statusCode(CREATED.getStatusCode())
+                .header(LOCATION, endsWith("/api/clients/3"));
+
+        final ProjectRequest project = new ProjectRequest("Pepito", true, "New project", 3L);
+        given()
+                .when()
+                .contentType(APPLICATION_JSON)
+                .body(project)
+                .post("/api/projects")
+                .then()
+                .statusCode(CREATED.getStatusCode())
+                .header(LOCATION, endsWith("/api/projects/4"));
+
+        final MemberRequest member = new MemberRequest(1L, Role.TeamLeader);
+        given()
+                .when()
+                .contentType(APPLICATION_JSON)
+                .body(member)
+                .post("/api/projects/4/members")
+                .then()
+                .statusCode(CREATED.getStatusCode())
+                .header(LOCATION, endsWith("/api/projects/4/members/5"));
+
+        final MemberRequest member2 = new MemberRequest(2L, Role.Developer);
+        given()
+                .when()
+                .contentType(APPLICATION_JSON)
+                .body(member2)
+                .post("/api/projects/4/members")
+                .then()
+                .statusCode(CREATED.getStatusCode())
+                .header(LOCATION, endsWith("/api/projects/4/members/6"));
+
+        final MemberResponse expectedMember = new MemberResponse(5L, 1L, Role.TeamLeader, 4L);
+        final MemberResponse expectedMember2 = new MemberResponse(6L, 2L, Role.Developer, 4L);
+        given()
+                .when()
+                .header(ACCEPT, APPLICATION_JSON)
+                .get("/api/members")
+                .then()
+                .statusCode(OK.getStatusCode())
+                .body(is(TestUtils.listOfTasJson(expectedMember, expectedMember2)));
     }
 }
