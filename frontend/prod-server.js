ee729a41a46c81b322fe02d34fa62ee31876209e
Nicolas Martignole <nicolas@touilleur-express.fr>
2020-04-23 17:42:22 +0200

    Tentative to add a Server for Clever

---
+++ frontend/prod-server.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const express = require('express')
+const app = express(),
+    DIST_DIR = __dirname + "/build/",
+    HTML_FILE = path.join(DIST_DIR, 'index.html')
+app.use(express.static(DIST_DIR))
+app.get('*', (req, res) => {
+    res.sendFile(HTML_FILE)
+})
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log(`TimeKeeper App started`)
+})
