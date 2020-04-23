9a506f97bec905f16296e8abacb68c633371e458
Nicolas Martignole <nicolas@touilleur-express.fr>
2020-04-23 18:47:36 +0200

    Use a lower version of Yarn for Clevercloud

--- frontend/prod-server.js
+++ frontend/prod-server.js
@@ -1,12 +1,12 @@
 const path = require('path')
 const express = require('express')
 const app = express(),
-    DIST_DIR = __dirname + "/",
+    DIST_DIR = __dirname + "/build/",
     HTML_FILE = path.join(DIST_DIR, 'index.html')
-app.use(express.static(DIST_DIR))
-app.get('*', (req, res) => {
-    res.sendFile(HTML_FILE)
-})
+    app.use(express.static(DIST_DIR))
+    app.get('*', (req, res) => {
+        res.sendFile(HTML_FILE)
+    })
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
     console.log(`TimeKeeper App started`)
