aa9658c94c088ea8a9f556719a86629379b06e9d
Nicolas Martignole <nicolas@touilleur-express.fr>
2020-04-23 17:50:48 +0200

    Use the root folder

--- frontend/prod-server.js
+++ frontend/prod-server.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const app = express(),
-    DIST_DIR = __dirname + "/build/",
+    DIST_DIR = __dirname + "/",
     HTML_FILE = path.join(DIST_DIR, 'index.html')
 app.use(express.static(DIST_DIR))
 app.get('*', (req, res) => {
