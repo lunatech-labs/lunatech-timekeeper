d65ca6c29a95f00716c58e7cdd2be9aeb24a3061
sfelix77 <30528315+sfelix77@users.noreply.github.com>
2020-04-27 09:20:14 +0200

    TK-114 : restricted access by profile in Quarkus (#64)
    * restricted access by profile in Quarkus + KeycloakTestResource to manage UnitTest with a TestContainer
    
    * add quarkus test port + remove default value in test/..../application.properties

--- src/test/java/fr/lunatech/timekeeper/resources/InfrastructureTest.java
+++ src/test/java/fr/lunatech/timekeeper/resources/InfrastructureTest.java
@@ -4,6 +4,7 @@ import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.h2.H2DatabaseTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.flywaydb.core.Flyway;
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,7 @@ import static javax.ws.rs.core.Response.Status.OK;
 
 @QuarkusTest
 @QuarkusTestResource(H2DatabaseTestResource.class)
+@QuarkusTestResource(KeycloakTestResource.class)
 @Tag("integration")
 class InfrastructureTest {
 
@@ -78,5 +80,11 @@ class InfrastructureTest {
                 .get("/health/ready")
                 .then()
                 .statusCode(OK.getStatusCode());
+    }
+
+    @Test
+    void shouldRetrieveTokensFromKeycloak() {
+        Assert.assertNotNull(KeycloakTestResource.getUserAccessToken());
+        Assert.assertNotNull(KeycloakTestResource.getAdminAccessToken());
     }
 }
