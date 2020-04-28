8c0b26099d154c10ed099b713251de1771231794
gdavy <guillaume.davy@lunatech.fr>
2020-04-28 14:40:21 +0200

    FindAllOrganizations Endpoint + TU

--- src/main/java/fr/lunatech/timekeeper/resources/openapi/OrganizationResourceApi.java
+++ src/main/java/fr/lunatech/timekeeper/resources/openapi/OrganizationResourceApi.java
@@ -1,6 +1,7 @@
 package fr.lunatech.timekeeper.resources.openapi;
 
 import fr.lunatech.timekeeper.services.dtos.OrganizationRequest;
+import fr.lunatech.timekeeper.services.dtos.OrganizationResponse;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.headers.Header;
@@ -10,18 +11,31 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
+import java.util.List;
+
 import static javax.ws.rs.core.HttpHeaders.LOCATION;
 
 @Path("/organizations")
 public interface OrganizationResourceApi {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Retrieve all organizations",
+            description = "Retrieve the list of organizations.")
+    @Tag(ref = "organizations")
+    @APIResponses(value = {
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Organizations retrieved"
+            )
+    })
+    List<OrganizationResponse> getAllOrganizations();
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
---
+++ src/test/java/fr/lunatech/timekeeper/resources/OrganizationResourceTest.java
@@ -0,0 +1,131 @@
+package fr.lunatech.timekeeper.resources;
+
+import fr.lunatech.timekeeper.services.dtos.ClientRequest;
+import fr.lunatech.timekeeper.services.dtos.ClientResponse;
+import fr.lunatech.timekeeper.services.dtos.OrganizationRequest;
+import fr.lunatech.timekeeper.services.dtos.OrganizationResponse;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static fr.lunatech.timekeeper.resources.KeycloakTestResource.getAdminAccessToken;
+import static fr.lunatech.timekeeper.resources.KeycloakTestResource.getUserAccessToken;
+import static fr.lunatech.timekeeper.resources.TestUtils.toJson;
+import static io.restassured.RestAssured.given;
+import static java.util.Collections.emptyList;
+import static javax.ws.rs.core.HttpHeaders.ACCEPT;
+import static javax.ws.rs.core.HttpHeaders.LOCATION;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.*;
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+@QuarkusTestResource(H2DatabaseTestResource.class)
+@QuarkusTestResource(KeycloakTestResource.class)
+@Tag("integration")
+public class OrganizationResourceTest {
+
+    @Inject
+    Flyway flyway;
+
+    @AfterEach
+    void cleanDB() {
+        flyway.clean();
+        flyway.migrate();
+    }
+
+    @Test
+    void shouldCreateOrganizationWhenAdminProfile() {
+
+        final String token = getAdminAccessToken();
+
+        final OrganizationRequest organization = new OrganizationRequest("New Organization", "New Token Name");
+        given()
+                .auth().preemptive().oauth2(token)
+                .when()
+                .contentType(APPLICATION_JSON)
+                .body(organization)
+                .post("/api/organizations")
+                .then()
+                .statusCode(CREATED.getStatusCode())
+                .header(LOCATION, endsWith("/api/organizations/1"));
+    }
+
+    @Test
+    void shouldNotCreateOrganizationWhenUserProfile() {
+
+        final String token = getUserAccessToken();
+
+        final OrganizationRequest organization = new OrganizationRequest("New Organization", "New Token Name");
+        given()
+                .auth().preemptive().oauth2(token)
+                .when()
+                .contentType(APPLICATION_JSON)
+                .body(organization)
+                .post("/api/organizations")
+                .then()
+                .statusCode(FORBIDDEN.getStatusCode());
+    }
+
+    @Test
+    void shouldNotFindUnknownOrganization() {
+
+        final String token = getUserAccessToken();
+
+        given()
+                .auth().preemptive().oauth2(token)
+                .when()
+                .header(ACCEPT, APPLICATION_JSON)
+                .get("/api/organizations/4")
+                .then()
+                .statusCode(NOT_FOUND.getStatusCode());
+    }
+
+    @Test
+    void shouldFindAllOrganizations() {
+
+        final String adminToken = getAdminAccessToken();
+        final String token = getUserAccessToken();
+
+        final OrganizationRequest organization = new OrganizationRequest("New Organization", "New Organization Token");
+        given()
+                .auth().preemptive().oauth2(adminToken)
+                .when()
+                .contentType(APPLICATION_JSON)
+                .body(organization)
+                .post("/api/organizations")
+                .then()
+                .statusCode(CREATED.getStatusCode())
+                .header(LOCATION, endsWith("/api/organizations/1"));
+
+        final OrganizationRequest organization2 = new OrganizationRequest("New Organization 2", "New Organization Token 2");
+        given()
+                .auth().preemptive().oauth2(adminToken)
+                .when()
+                .contentType(APPLICATION_JSON)
+                .body(organization2)
+                .post("/api/organizations")
+                .then()
+                .statusCode(CREATED.getStatusCode())
+                .header(LOCATION, endsWith("/api/organizations/2"));
+
+        final OrganizationResponse expectedOrganization = new OrganizationResponse(1L, "New Organization", "New Organization Token", emptyList(), emptyList());
+        final OrganizationResponse expectedOrganization2 = new OrganizationResponse(2L, "New Organization 2", "New Organization Token 2", emptyList(), emptyList());
+        given()
+                .auth().preemptive().oauth2(token)
+                .when()
+                .header(ACCEPT, APPLICATION_JSON)
+                .get("/api/organizations")
+                .then()
+                .statusCode(OK.getStatusCode())
+                .body(is(TestUtils.listOfTasJson(expectedOrganization, expectedOrganization2)));
+    }
+
+}
