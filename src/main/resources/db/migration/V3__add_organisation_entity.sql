6200eb11578b6ce597472b26707ff2df598ef583
Fabrice Sznajderman <fabszn@protonmail.com>
2020-04-28 14:48:42 +0200

    fix

--- src/main/resources/db/migration/V3__add_organisation_entity.sql
+++ src/main/resources/db/migration/V3__add_organisation_entity.sql
@@ -6,10 +6,17 @@ CREATE TABLE public.organizations (
 );
 
 
-ALTER TABLE public.users add organization_id int8 not null;
+insert into public.organisation (id, name, tokenname) values (1,'Lunatech NL', 'lunatech.nl');
+insert into public.organisation (id, name, tokenname) values (2,'Lunatech FR', 'lunatech.fr');
+
+ALTER TABLE public.users add organization_id int8 not null default 1 ;
 
 ALTER TABLE public.users ADD constraint fk_users_organization_id foreign KEY  (organization_id) REFERENCES public.organizations (id);
 
 ALTER TABLE public.projects add organization_id int8 not null;
 
 ALTER TABLE public.projects ADD constraint fk_projects_oranisation_id FOREIGN KEY (organization_id) REFERENCES public.organizations (id);
+
+update users set organisation_id=1 where true = starts_with(reverse(email),'ln');
+
+update users set organisation_id=2 where true = starts_with(reverse(email),'fr');
