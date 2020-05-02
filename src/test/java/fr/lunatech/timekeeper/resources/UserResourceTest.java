ae2af6e3ab0df6436604c908183fef77f1b6c108
Fabrice Sznajderman <fabszn@protonmail.com>
2020-05-02 22:00:41 +0200

    six

--- src/test/java/fr/lunatech/timekeeper/resources/UserResourceTest.java
+++ src/test/java/fr/lunatech/timekeeper/resources/UserResourceTest.java
@@ -12,14 +12,12 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
-
 import java.util.List;
 
 import static fr.lunatech.timekeeper.models.Profile.Admin;
 import static fr.lunatech.timekeeper.models.Profile.User;
 import static fr.lunatech.timekeeper.resources.KeycloakTestResource.getAdminAccessToken;
 import static fr.lunatech.timekeeper.resources.KeycloakTestResource.getUserAccessToken;
-import static fr.lunatech.timekeeper.resources.utils.TestUtils.createUserRequest;
 import static fr.lunatech.timekeeper.resources.utils.TestUtils.toJson;
 import static io.restassured.RestAssured.given;
 import static java.util.Collections.emptyList;
@@ -62,7 +60,7 @@ class UserResourceTest {
                 .statusCode(CREATED.getStatusCode())
                 .header(LOCATION, endsWith("/api/organizations/1"));
 
-        final UserRequest user = createUserRequest("Sam", "Huel", "sam@gmail.com", "sam.png", Admin);
+        final UserRequest user = new UserRequest("Sam", "Huel", "sam@gmail.com", "sam.png", List.of(Admin));
         given()
                 .auth().preemptive().oauth2(adminToken)
                 .when()
@@ -102,7 +100,7 @@ class UserResourceTest {
                 .statusCode(CREATED.getStatusCode())
                 .header(LOCATION, endsWith("/api/organizations/1"));
 
-        final UserRequest user = createUserRequest("Sam", "Huel", "sam@gmail.com", "sam.png", Admin);
+        final UserRequest user = new UserRequest("Sam", "Huel", "sam@gmail.com", "sam.png", List.of(Admin));
         given()
                 .auth().preemptive().oauth2(token)
                 .when()
@@ -113,7 +111,7 @@ class UserResourceTest {
                 .statusCode(CREATED.getStatusCode())
                 .header(LOCATION, endsWith("/api/users/2"));
 
-        final UserRequest user2 = createUserRequest("Sam2", "Huel2", "sam@gmail.com", "sam.png", Admin);
+        final UserRequest user2 = new UserRequest("Sam2", "Huel2", "sam@gmail.com", "sam.png", List.of(Admin));
         given()
                 .auth().preemptive().oauth2(token)
                 .when()
@@ -129,7 +127,7 @@ class UserResourceTest {
 
         final String token = getUserAccessToken();
 
-        final UserRequest user = createUserRequest("Sam", "Huel", "sam@gmail.com", "sam.png", Admin);
+        final UserRequest user = new UserRequest("Sam", "Huel", "sam@gmail.com", "sam.png", List.of(Admin));
         given()
                 .auth().preemptive().oauth2(token)
                 .when()
@@ -170,7 +168,7 @@ class UserResourceTest {
                 .statusCode(CREATED.getStatusCode())
                 .header(LOCATION, endsWith("/api/organizations/1"));
 
-        final UserRequest user1 = createUserRequest("Sam", "Huel", "sam@gmail.com", "sam.png", Admin);
+        final UserRequest user1 = new UserRequest("Sam", "Huel", "sam@gmail.com", "sam.png", List.of(Admin));
         given()
                 .auth().preemptive().oauth2(token)
                 .when()
@@ -181,7 +179,7 @@ class UserResourceTest {
                 .statusCode(CREATED.getStatusCode())
                 .header(LOCATION, endsWith("/api/users/2"));
 
-        final UserRequest user2 = createUserRequest("Sam2", "Huel2", "sam@gmail.com", "sam2.png", User);
+        final UserRequest user2 = new UserRequest("Sam2", "Huel2", "sam@gmail.com", "sam2.png", List.of(User));
         given()
                 .auth().preemptive().oauth2(token)
                 .when()
@@ -220,7 +218,7 @@ class UserResourceTest {
                 .statusCode(CREATED.getStatusCode())
                 .header(LOCATION, endsWith("/api/organizations/1"));
 
-        final UserRequest user1 = createUserRequest("Sam", "Huel", "sam@gmail.com", "sam.png", Admin);
+        final UserRequest user1 = new UserRequest("Sam", "Huel", "sam@gmail.com", "sam.png", List.of(Admin));
         given()
                 .auth().preemptive().oauth2(token)
                 .when()
@@ -231,7 +229,7 @@ class UserResourceTest {
                 .statusCode(CREATED.getStatusCode())
                 .header(LOCATION, endsWith("/api/users/2"));
 
-        final UserRequest user2 = createUserRequest("Sam2", "Huel2", "sam2@gmail.com", "sam2.png", User);
+        final UserRequest user2 = new UserRequest("Sam2", "Huel2", "sam2@gmail.com", "sam2.png", List.of(User));
         given()
                 .auth().preemptive().oauth2(token)
                 .when()
@@ -259,7 +257,7 @@ class UserResourceTest {
                 .statusCode(CREATED.getStatusCode())
                 .header(LOCATION, endsWith("/api/organizations/1"));
 
-        final UserRequest user1 = createUserRequest("Sam", "Huel", "sam@gmail.com", "sam.png", Admin);
+        final UserRequest user1 = new UserRequest("Sam", "Huel", "sam@gmail.com", "sam.png", List.of(Admin));
         given()
                 .auth().preemptive().oauth2(adminToken)
                 .when()
@@ -270,7 +268,7 @@ class UserResourceTest {
                 .statusCode(CREATED.getStatusCode())
                 .header(LOCATION, endsWith("/api/users/2"));
 
-        final UserRequest user2 = createUserRequest("Sam2", "Huel2", "sam@gmail.com", "sam2.png", User);
+        final UserRequest user2 = new UserRequest("Sam2", "Huel2", "sam@gmail.com", "sam2.png", List.of(User));
         given()
                 .auth().preemptive().oauth2(token)
                 .when()
@@ -298,7 +296,7 @@ class UserResourceTest {
                 .statusCode(CREATED.getStatusCode())
                 .header(LOCATION, endsWith("/api/organizations/1"));
 
-        final UserRequest user1 = createUserRequest("Sam", "Huel", "sam@gmail.com", "sam.png", Admin);
+        final UserRequest user1 = new UserRequest("Sam", "Huel", "sam@gmail.com", "sam.png", List.of(Admin));
         given()
                 .auth().preemptive().oauth2(adminToken)
                 .when()
@@ -309,7 +307,7 @@ class UserResourceTest {
                 .statusCode(CREATED.getStatusCode())
                 .header(LOCATION, endsWith("/api/users/2"));
 
-        final UserRequest user2 = createUserRequest("Sam2", "Huel2", "sam2@gmail.com", "sam2.png", User);
+        final UserRequest user2 = new UserRequest("Sam2", "Huel2", "sam2@gmail.com", "sam2.png", List.of(User));
         given()
                 .auth().preemptive().oauth2(adminToken)
                 .when()
@@ -366,7 +364,7 @@ class UserResourceTest {
                 .statusCode(CREATED.getStatusCode())
                 .header(LOCATION, endsWith("/api/organizations/1"));
 
-        final UserRequest user1 = createUserRequest("Sam", "Huel", "sam@gmail.com", "sam.png", Admin);
+        final UserRequest user1 = new UserRequest("Sam", "Huel", "sam@gmail.com", "sam.png", List.of(Admin));
         given()
                 .auth().preemptive().oauth2(adminToken)
                 .when()
@@ -377,7 +375,7 @@ class UserResourceTest {
                 .statusCode(CREATED.getStatusCode())
                 .header(LOCATION, endsWith("/api/users/2"));
 
-        final UserRequest user2 = createUserRequest("Jimmy", "Pastore", "jimmy@gmail.com", "jimmy.png", User);
+        final UserRequest user2 = new UserRequest("Jimmy", "Pastore", "jimmy@gmail.com", "jimmy.png", List.of(User));
         given()
                 .auth().preemptive().oauth2(adminToken)
                 .when()
--- src/test/java/fr/lunatech/timekeeper/resources/utils/TestUtils.java
+++ src/test/java/fr/lunatech/timekeeper/resources/utils/TestUtils.java
@@ -1,13 +1,13 @@
 package fr.lunatech.timekeeper.resources.utils;
 
-import fr.lunatech.timekeeper.models.Profile;
-import fr.lunatech.timekeeper.services.dtos.UserRequest;
-
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * TestUtils provides methods that ease
+ */
 public class TestUtils {
 
     static Jsonb jsonb = JsonbBuilder.create();
@@ -16,33 +16,16 @@ public class TestUtils {
         return jsonb.toJson(o);
     }
 
-    public static UserRequest createUserRequest(String firstName, String lastName, String email, String picture, Profile... profiles) {
-        final List<Profile> p = Arrays.asList(profiles);
-        return new UserRequest(firstName, lastName, email, picture, p);
-    }
-
-    @SafeVarargs
-    public static <T> List<T> listOf(T... elements) {
-        return Arrays.<T>asList(elements);
-
-    }
-
-
-
     @SafeVarargs
     public static <T> String listOfTasJson(T... elements) {
         return toJson(Arrays.<T>asList(elements));
-
-
     }
-
 
     public static <T> String listOfTasJson(List<T> elements) {
         return toJson(elements);
-
     }
+
     public static <T> String listOfTasJson(io.vavr.collection.List<T> elements) {
         return toJson(elements.asJava());
-
     }
 }
