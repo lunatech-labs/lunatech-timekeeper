02277c0e6140516e4974cfa6c26595a4556610d0
thierry kuy <kuy.thierry@gmail.com>
2020-05-04 16:43:20 +0200

    Remove the ClientList.less because it is useless

--- frontend/src/components/Clients/ClientList.js
+++ frontend/src/components/Clients/ClientList.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {Alert, AutoComplete, Avatar, Button, Card, Collapse, List, Spin} from 'antd';
 import logo from '../../img/logo_timekeeper_homepage.png';
-import './ClientList.less';
+import '../../App.less';
 import {useTimeKeeperAPI} from '../../utils/services';
 import FolderFilled from '@ant-design/icons/lib/icons/FolderFilled';
 import EditFilled from '@ant-design/icons/lib/icons/EditFilled';
