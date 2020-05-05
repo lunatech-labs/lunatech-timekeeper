a208046d2297b8e7e77791db3b0246f37abdc3e6
SFE <30528315+sfelix77@users.noreply.github.com>
2020-05-05 02:14:08 +0200

    move page content in page component

--- frontend/src/components/Projects/ProjectList.js
+++ frontend/src/components/Projects/ProjectList.js
@@ -1,20 +1,12 @@
 import React from 'react';
 
-import {Button, PageHeader} from 'antd';
-import {Link} from 'react-router-dom';
-import PlusOutlined from '@ant-design/icons/lib/icons/PlusOutlined';
 import './ProjectList.less';
 
 
 const ProjectList = () => {
   return (
     <React.Fragment>
-      <div className="tk_TopPage">
-        <PageHeader title="Projects"/>
-        <div id="add-button">
-          <Link key='addLink' to='/projects/new'><Button type="primary" icon={<PlusOutlined/>}>Add a project</Button></Link>
-        </div>
-      </div>
+
     </React.Fragment>
   );
 };
