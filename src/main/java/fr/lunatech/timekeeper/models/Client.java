3a919af647ddae28ffb575f95b374a7bee9f3b45
SFE <30528315+sfelix77@users.noreply.github.com>
2020-05-07 13:36:03 +0200

    add relation between client and organization

--- src/main/java/fr/lunatech/timekeeper/models/Client.java
+++ src/main/java/fr/lunatech/timekeeper/models/Client.java
@@ -2,10 +2,7 @@ package fr.lunatech.timekeeper.models;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.List;
@@ -13,6 +10,11 @@ import java.util.List;
 @Entity
 @Table(name = "clients", uniqueConstraints = {@UniqueConstraint(columnNames = {"name"})})
 public class Client extends PanacheEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "organization_id", nullable = false)
+    @NotNull
+    public Organization organization;
 
     @NotBlank
     public String name;
