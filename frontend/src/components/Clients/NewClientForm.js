7ca1847eb8046b300f157de6761dd9b3a2e70eef
sfelix77 <30528315+sfelix77@users.noreply.github.com>
2020-05-08 21:32:26 +0200

    fix undefined field in form (#101)
    => Jsonb could accept a null value but in this case, to simplify the code in backend, we determine all strings non nullable

--- frontend/src/components/Clients/NewClientForm.js
+++ frontend/src/components/Clients/NewClientForm.js
@@ -13,6 +13,11 @@ const tailLayout = {
   },
 };
 
+const initialValues = {
+  name: "",
+  description: ""
+};
+
 const ClientForm = () => {
 
   const [clientCreated, setClientCreated] = useState(false);
@@ -59,6 +64,7 @@ const ClientForm = () => {
         wrapperCol={{span: 14}}
         layout="horizontal"
         onFinish={timeKeeperAPIPost.run}
+        initialValues={initialValues}
       >
         <Form.Item
           label="Name"
