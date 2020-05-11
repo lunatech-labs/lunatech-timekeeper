b3f6a256d88e7b547d847268b4b143a70b6bee2e
Guillaume <guillaume.davy@lunatech.fr>
2020-05-11 10:01:59 +0200

    frontend projects edit url + eslint changes

---
+++ frontend/src/pages/Project/editProjects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import MainPage from '../MainPage/MainPage';
+
+const EditProjectsPage = () => {
+  return (
+    <MainPage title="Edit project">
+    </MainPage>
+  );
+};
+
+export default EditProjectsPage;
