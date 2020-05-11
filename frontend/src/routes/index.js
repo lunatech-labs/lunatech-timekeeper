fb0becb5bc448c565f0a7651b7c890f1c43ba871
Guillaume <guillaume.davy@lunatech.fr>
2020-05-11 16:07:07 +0200

    Line commented

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -36,7 +36,7 @@ export const AppRouter = () => {
         <PrivateRoute path="/clients/new"  component={NewClientPage} roles={['admin']} />
         <PrivateRoute path="/clients/:id/edit"  component={EditClientPage} roles={['admin']} />
         <PrivateRoute path="/clients"      component={ClientsPage} roles={['admin']} />
-         the /clients route must be after any other clients routes, else it does not work
+         {/*the /clients route must be after any other clients routes, else it does not work*/}
         <Redirect from="/" to="/home" />
       </Switch>
     </Router>
