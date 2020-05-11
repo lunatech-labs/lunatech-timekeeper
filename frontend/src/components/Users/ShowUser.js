4c04efaebc17c8c2b01f3c6d00fe7d22495cec77
Nicolas Martignole <nicolas@touilleur-express.fr>
2020-05-11 16:17:41 +0200

    Fix to display the name

--- frontend/src/components/Users/ShowUser.js
+++ frontend/src/components/Users/ShowUser.js
@@ -12,8 +12,7 @@ class ShowUser extends Component {
         bordered
         column={{ xxl: 1, xl: 1, lg: 1, md: 1, sm: 1, xs: 1 }}>
         <Descriptions.Item label="Avatar"><Avatar src={this.props.user.picture} /></Descriptions.Item>
-        <Descriptions.Item label="First name"> {this.props.user.firstName}</Descriptions.Item>
-        <Descriptions.Item label="Last name"> {this.props.user.lastName}</Descriptions.Item>
+        <Descriptions.Item label="Full name"> {this.props.user.name}</Descriptions.Item>
         <Descriptions.Item label="Email"> {this.props.user.email}</Descriptions.Item>
         <Descriptions.Item label="Profiles"> {this.props.user.profiles.join(', ')}</Descriptions.Item>
       </Descriptions>
