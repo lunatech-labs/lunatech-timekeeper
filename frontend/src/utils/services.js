5d8fa1d0493a076ebb4b18085eb6996f30f7488f
thierry kuy <kuy.thierry@gmail.com>
2020-05-12 10:42:12 +0200

    linter

--- frontend/src/utils/services.js
+++ frontend/src/utils/services.js
@@ -4,78 +4,78 @@ import {useRequest} from '@umijs/hooks';
 // This must imported else the Request will not use the Bearer
 // eslint-disable-line no-unused-vars
 import request from './request';
-import {notification} from "antd";
+import {notification} from 'antd';
 
 const codeMessage = {
-    200: 'Ok',
-    201: 'Created',
-    204: 'No Content',
-    400: 'Bad Request',
-    401: 'Unauthorized',
-    403: 'Forbidden',
-    404: 'Resource not found',
-    500: 'Server Error',
+  200: 'Ok',
+  201: 'Created',
+  204: 'No Content',
+  400: 'Bad Request',
+  401: 'Unauthorized',
+  403: 'Forbidden',
+  404: 'Resource not found',
+  500: 'Server Error',
 };
 
 function errorHandler(error) {
-    const {response} = error;
+  const {response} = error;
 
-    if (response && response.status) {
-        const contentType = response.headers.get("content-type");
-        if (contentType && contentType.indexOf("application/json") !== -1) {
-            const promiseResult = response.json();
+  if (response && response.status) {
+    const contentType = response.headers.get('content-type');
+    if (contentType && contentType.indexOf('application/json') !== -1) {
+      const promiseResult = response.json();
 
-            promiseResult.then(function (bodyTxt) {
-                console.log(bodyTxt.message);
-                const errorText = 'Message from server: ' + bodyTxt.message;
-                const {status, url} = response;
+      promiseResult.then(function (bodyTxt) {
+        console.log(bodyTxt.message);
+        const errorText = 'Message from server: ' + bodyTxt.message;
+        const {status, url} = response;
 
-                notification.error({
-                    message: `Server error ${status}: ${url}`,
-                    description: errorText,
-                });
-            })
-        } else {
-            return response.text().then(text => {
-                const errorText = codeMessage[response.status] || response.statusText + ' ' + text;
-                const {status, url} = response;
-                notification.error({
-                    message: `Server error ${status}: ${url}`,
-                    description: errorText,
-                });
-            });
+        notification.error({
+          message: `Server error ${status}: ${url}`,
+          description: errorText,
+        });
+      });
+    } else {
+      return response.text().then(text => {
+        const errorText = codeMessage[response.status] || response.statusText + ' ' + text;
+        const {status, url} = response;
+        notification.error({
+          message: `Server error ${status}: ${url}`,
+          description: errorText,
+        });
+      });
 
-        }
     }
+  }
 
-    return response;
+  return response;
 }
 
 export const useTimeKeeperAPI = (urlAPI, ...rest) => {
-    const [keycloak, initialized] = useKeycloak();
+  const [keycloak, initialized] = useKeycloak();
 
-    // Do a dummy usage of request to avoid a warn at compile
-    // request must be imported to override the useRequest default `request`
-    // NMA
-    if (request.name == null) {
-        console.log('Error: request should be set to umiInstance');
+  // Do a dummy usage of request to avoid a warn at compile
+  // request must be imported to override the useRequest default `request`
+  // NMA
+  if (request.name == null) {
+    console.log('Error: request should be set to umiInstance');
+  }
+
+  useEffect(() => {
+    if (initialized) {
+      localStorage.setItem('x-auth-token', keycloak.token);
     }
+  }, [urlAPI, initialized, keycloak]
+  );
 
-    useEffect(() => {
-            if (initialized) {
-                localStorage.setItem('x-auth-token', keycloak.token);
-            }
-        }, [urlAPI, initialized, keycloak]
-    );
+  const toReturn = useRequest(process.env.REACT_APP_QUARKUS_BACKEND + urlAPI, ...rest);
 
-    const toReturn = useRequest(process.env.REACT_APP_QUARKUS_BACKEND + urlAPI, ...rest);
+  if (toReturn.error) {
+    console.log('got an error');
+    errorHandler(toReturn.error);
+  }
 
-    if (toReturn.error) {
-        console.log("got an error")
-        errorHandler(toReturn.error);
-    }
-
-    return toReturn;
+  return toReturn;
 
 };
 
@@ -87,53 +87,53 @@ export const useTimeKeeperAPI = (urlAPI, ...rest) => {
  * @param booleanCallback : a function that accepts a boolean, to indicate if it was successful
  */
 export const useTimeKeeperAPIPost = (urlAPI, formData, booleanCallback) => {
-    const [keycloak, initialized] = useKeycloak();
+  const [keycloak, initialized] = useKeycloak();
 
-    useEffect(() => {
-            if (initialized) {
-                localStorage.setItem('x-auth-token', keycloak.token);
-            }
-        }, [urlAPI, initialized, keycloak]
-    );
+  useEffect(() => {
+    if (initialized) {
+      localStorage.setItem('x-auth-token', keycloak.token);
+    }
+  }, [urlAPI, initialized, keycloak]
+  );
 
-    return useRequest((formData) => ({
-        url: process.env.REACT_APP_QUARKUS_BACKEND + urlAPI,
-        method: 'post',
-        data: formData
-    }), {
-        manual: true,
-        onSuccess: () => {
-            if (booleanCallback) {
-                booleanCallback(true);
-            } else {
-                console.error('Err: please set a callback for POST call to ' + urlAPI);
-            }
-        }
-    });
+  return useRequest((formData) => ({
+    url: process.env.REACT_APP_QUARKUS_BACKEND + urlAPI,
+    method: 'post',
+    data: formData
+  }), {
+    manual: true,
+    onSuccess: () => {
+      if (booleanCallback) {
+        booleanCallback(true);
+      } else {
+        console.error('Err: please set a callback for POST call to ' + urlAPI);
+      }
+    }
+  });
 };
 
 export const useTimeKeeperAPIPut = (urlAPI, formData, booleanCallback) => {
-    const [keycloak, initialized] = useKeycloak();
+  const [keycloak, initialized] = useKeycloak();
 
-    useEffect(() => {
-            if (initialized) {
-                localStorage.setItem('x-auth-token', keycloak.token);
-            }
-        }, [urlAPI, initialized, keycloak]
-    );
+  useEffect(() => {
+    if (initialized) {
+      localStorage.setItem('x-auth-token', keycloak.token);
+    }
+  }, [urlAPI, initialized, keycloak]
+  );
 
-    return useRequest((formData) => ({
-        url: process.env.REACT_APP_QUARKUS_BACKEND + urlAPI,
-        method: 'put',
-        data: formData
-    }), {
-        manual: true,
-        onSuccess: () => {
-            if (booleanCallback) {
-                booleanCallback(true);
-            } else {
-                console.error('Err: please set a callback for PUT call to ' + urlAPI);
-            }
-        }
-    });
+  return useRequest((formData) => ({
+    url: process.env.REACT_APP_QUARKUS_BACKEND + urlAPI,
+    method: 'put',
+    data: formData
+  }), {
+    manual: true,
+    onSuccess: () => {
+      if (booleanCallback) {
+        booleanCallback(true);
+      } else {
+        console.error('Err: please set a callback for PUT call to ' + urlAPI);
+      }
+    }
+  });
 };
