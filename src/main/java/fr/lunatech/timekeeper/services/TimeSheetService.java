c30227fe53f94adc97028fcee35cef98c514eac1
thierry kuy <kuy.thierry@gmail.com>
2020-05-25 15:02:57 +0200

    Add timesheet for the members of the project

--- src/main/java/fr/lunatech/timekeeper/services/TimeSheetService.java
+++ src/main/java/fr/lunatech/timekeeper/services/TimeSheetService.java
@@ -1,18 +1,42 @@
 package fr.lunatech.timekeeper.services;
 
-import fr.lunatech.timekeeper.models.Project;
 import fr.lunatech.timekeeper.models.time.TimeSheet;
+import fr.lunatech.timekeeper.resources.exceptions.CreateResourceException;
+import fr.lunatech.timekeeper.services.requests.TimeSheetRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.PersistenceException;
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @ApplicationScoped
 public class TimeSheetService {
 
+    @Inject
+    UserService userService;
+
+    @Inject
+    ProjectService projectService;
+
+    private static Logger logger = LoggerFactory.getLogger(TimeSheetService.class);
+
     Optional<TimeSheet> findById(Long id) {
         return TimeSheet.findByIdOptional(id);
+    }
+
+    @Transactional
+    Long createTimeSheet(TimeSheetRequest request, AuthenticationContext ctx) {
+        logger.debug("Create a new timesheet with {}, {}", request, ctx);
+        final TimeSheet timeSheet = request.unbind(projectService::findById, userService::findById, ctx);
+        try {
+            timeSheet.persistAndFlush();
+        } catch (PersistenceException pe) {
+            throw new CreateResourceException(String.format("Timesheet was not created due to constraint violation"));
+        }
+        return timeSheet.id;
     }
 
 }
---
+++ src/main/java/fr/lunatech/timekeeper/services/requests/TimeSheetRequest.java
@@ -0,0 +1,94 @@
+package fr.lunatech.timekeeper.services.requests;
+
+import fr.lunatech.timekeeper.models.Project;
+import fr.lunatech.timekeeper.models.User;
+import fr.lunatech.timekeeper.models.time.TimeSheet;
+import fr.lunatech.timekeeper.services.AuthenticationContext;
+import fr.lunatech.timekeeper.services.exceptions.IllegalEntityStateException;
+import fr.lunatech.timekeeper.timeutils.TimeUnit;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.function.BiFunction;
+
+public class TimeSheetRequest {
+    @NotNull
+    public Long projectId;
+
+    @NotNull
+    public Long ownerId;
+
+    @Enumerated(EnumType.STRING)
+    public TimeUnit timeUnit;
+
+    public Boolean defaultIsBillable;
+
+    // 1---------------
+    @Null
+    public LocalDate expirationDate;
+
+    // 2 ------------------
+    @Null
+    public Integer maxDuration; // eg 21
+
+    @Null
+    public String durationUnit; // DAYS
+
+    public TimeSheetRequest(@NotNull Long projectId, @NotNull Long ownerId, TimeUnit timeUnit, Boolean defaultIsBillable, @Null LocalDate expirationDate, @Null Integer maxDuration, @Null String durationUnit) {
+        this.projectId = projectId;
+        this.ownerId = ownerId;
+        this.timeUnit = timeUnit;
+        this.defaultIsBillable = defaultIsBillable;
+        this.expirationDate = expirationDate;
+        this.maxDuration = maxDuration;
+        this.durationUnit = durationUnit;
+    }
+
+    public TimeSheet unbind(
+            @NotNull BiFunction<Long, AuthenticationContext, Optional<Project>> findProject,
+            @NotNull BiFunction<Long, AuthenticationContext, Optional<User>> findOwner,
+            @NotNull AuthenticationContext ctx
+    ) {
+        TimeSheet timeSheet = new TimeSheet();
+        timeSheet.project = findProject.apply(getProjectId(), ctx).orElseThrow(() -> new IllegalEntityStateException("Project not found for id " + getProjectId()));
+        timeSheet.owner = findOwner.apply(getOwnerId(), ctx).orElseThrow(() -> new IllegalEntityStateException("Owner not found for id " + getProjectId()));
+        timeSheet.timeUnit = getTimeUnit();
+        timeSheet.defaultIsBillable = getDefaultIsBillable();
+        timeSheet.expirationDate = getExpirationDate();
+        timeSheet.maxDuration = getMaxDuration();
+        timeSheet.durationUnit = getDurationUnit();
+        return timeSheet;
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public TimeUnit getTimeUnit() {
+        return timeUnit;
+    }
+
+    public Boolean getDefaultIsBillable() {
+        return defaultIsBillable;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public Integer getMaxDuration() {
+        return maxDuration;
+    }
+
+    public String getDurationUnit() {
+        return durationUnit;
+    }
+}
