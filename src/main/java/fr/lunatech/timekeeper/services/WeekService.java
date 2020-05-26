e08d12711cde2496f532e11ca455a9b1ebec9453
Nicolas Martignole <nicolas@touilleur-express.fr>
2020-05-26 11:20:41 +0200

    Create a mock WeekResponse to test the API

--- src/main/java/fr/lunatech/timekeeper/services/WeekService.java
+++ src/main/java/fr/lunatech/timekeeper/services/WeekService.java
@@ -1,10 +1,17 @@
 package fr.lunatech.timekeeper.services;
 
+import fr.lunatech.timekeeper.models.time.TimeSheet;
+import fr.lunatech.timekeeper.models.time.UserEvent;
+import fr.lunatech.timekeeper.services.responses.TimeSheetResponse;
 import fr.lunatech.timekeeper.services.responses.WeekResponse;
+import fr.lunatech.timekeeper.timeutils.CalendarFR2020;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @ApplicationScoped
@@ -15,10 +22,29 @@ public class WeekService {
 
         // TODO
         logger.warn("TODO getCurrentWeek");
-        //return Week.find("users", userId);
+
+        var userEvents = new ArrayList<UserEvent>();
+
+        // Fake DATA - Temporary
+        var nicolasDaysOff = new UserEvent();
+        nicolasDaysOff.startDateTime=LocalDate.of(2020,05,22).atStartOfDay();
+        nicolasDaysOff.endDateTime=LocalDate.of(2020,05,23).atStartOfDay();
+        nicolasDaysOff.description="Journée de congès";
+        nicolasDaysOff.id=1L;
+
+        // Fake DATA - Temporary
+        var sheets = new ArrayList<TimeSheetResponse>();
+
+        var publicHolidays = new CalendarFR2020().getPublicHolidays();
+
+        // Fake DATA - Temporary
+        WeekResponse weekResponse = new WeekResponse(LocalDate.of(2020,05,18)
+        , userEvents
+        , sheets
+        , publicHolidays);
 
 
-        return Optional.empty();
+        return Optional.ofNullable(weekResponse);
 
     }
 }
--- src/main/java/fr/lunatech/timekeeper/services/responses/WeekResponse.java
+++ src/main/java/fr/lunatech/timekeeper/services/responses/WeekResponse.java
@@ -2,6 +2,9 @@ package fr.lunatech.timekeeper.services.responses;
 
 import fr.lunatech.timekeeper.models.time.UserEvent;
 import fr.lunatech.timekeeper.models.time.TimeSheet;
+import fr.lunatech.timekeeper.timeutils.Calendar;
+import fr.lunatech.timekeeper.timeutils.CalendarFR2020;
+import fr.lunatech.timekeeper.timeutils.PublicHoliday;
 import fr.lunatech.timekeeper.timeutils.Week;
 
 import javax.validation.constraints.NotBlank;
@@ -10,33 +13,36 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 public final class WeekResponse {
 
     private final LocalDate firstDayOfWeek;
 
-    @NotNull
     private final List<UserEvent> userEvents;
 
     private final List<TimeSheetResponse> sheets;
 
+    private final List<PublicHoliday> publicHolidays;
+
     public WeekResponse(
             @NotBlank LocalDate firstDayOfWeek,
             @NotNull List<UserEvent> userEvents,
-            @NotNull List<TimeSheetResponse> sheets
+            @NotNull List<TimeSheetResponse> sheets,
+            @NotNull List<PublicHoliday> publicHolidays
     ) {
         this.firstDayOfWeek = firstDayOfWeek;
         this.userEvents = userEvents;
         this.sheets = sheets;
+        this.publicHolidays=publicHolidays;
     }
 
-    public static WeekResponse bind(@NotNull Week week, List<TimeSheet> sheets) {
+    public static WeekResponse bind(@NotNull Week week, List<TimeSheet> sheets, List<PublicHoliday> publicHolidays) {
         return new WeekResponse(
                 week.firstDayOfWeek,
                 week.userEvents,
                 sheets.stream()
                 .map(TimeSheetResponse::bind)
-                .collect(Collectors.toList())
+                .collect(Collectors.toList()),
+                publicHolidays
         );
     }
 
