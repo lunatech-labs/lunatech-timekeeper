24d27325b703d87f7daf2119fd1b5eda59be73d4
thierry kuy <kuy.thierry@gmail.com>
2020-05-28 11:14:14 +0200

    Use a list of data

--- frontend/src/pages/TimeSheet/timeEntriesPage.js
+++ frontend/src/pages/TimeSheet/timeEntriesPage.js
@@ -6,23 +6,51 @@ import {Badge} from "antd";
 const moment = require('moment');
 
 const TimeEntriesPage = () => {
-  const firstDayOfWeek = moment().startOf('week').add(1, 'day');
+  const firstDayOfWeek = moment().startOf('week').add(1, 'day'); //TODO : Locale
+
+  const staticData = [
+    {
+      date: firstDayOfWeek,
+      disabled: true,
+      data: [{
+        name: "First Day of the week is a day off",
+      }]
+    },
+    {
+      date: firstDayOfWeek.clone().add(2, 'day'),
+      disabled: false,
+      data: [{
+        name: "It is wednesday my dudes",
+        description: "First element of the day",
+        dateTime: firstDayOfWeek.clone().add(4, 'hour')
+      }, {
+        name: "It is wednesday my dudes",
+        description: "Second element of the day",
+        dateTime: firstDayOfWeek.clone().add(8, 'hour')
+      }]
+    }
+  ]
   return (
     <MainPage title="Time entries">
       <WeekCalendar
         firstDay={firstDayOfWeek}
-        dateCellRender={(data) => {
+        dateCellRender={(data, date, disabled) => {
           return (
-            <div><Badge status={(!!data && !!data.name) ? 'success' : 'error'} text={(data && data.name) || 'Nothing to render' } /></div>
+            <div>
+              {data.map(entry => {
+                return (
+                  <div>
+                    <Badge
+                      status={(entry && entry.name) ? 'success' : 'error'}
+                      text={(entry && entry.name) ? `name : ${entry.name} ${entry.dateTime && `(${entry.dateTime.format('hh:mm')})`}` : 'Nothing to render'}
+                    />
+                  </div>
+                )
+              })}
+            </div>
           )
         }}
-        days={[{
-          moment:firstDayOfWeek,
-          disabled: true,
-          data: {
-            name: "First Day of the week"
-          }
-        }]}
+        days={staticData}
       />
     </MainPage>
   );
