4fa2878a3ea818cf1022976fe57a12cb8f06ecce
thierry kuy <kuy.thierry@gmail.com>
2020-06-02 16:27:41 +0200

    Change import

--- frontend/src/components/Projects/ShowProject.js
+++ frontend/src/components/Projects/ShowProject.js
@@ -7,9 +7,9 @@ import TitleSection from '../Title/TitleSection';
 import CardLg from '../Card/CardLg';
 import CardMember from '../Card/CardMember';
 import TagMember from '../Tag/TagMember';
-import Tooltip from 'antd/es/tooltip';
 import TagProjectClient from '../Tag/TagProjectClient';
 import ShowTimeSheet from '../TimeSheet/ShowTimeSheet';
+import Tooltip from 'antd/lib/tooltip';
 import Separator from '../Separator/Separator';
 
 const {Title} = Typography;
