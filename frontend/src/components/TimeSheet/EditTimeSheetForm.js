ded1311117969d5b6dcd2407067e61ec35df9ccd
dix <francois.dixmeras@lunatech.fr>
2020-06-10 15:10:31 +0200

    fix datePicker init value + format code

--- frontend/src/components/TimeSheet/EditTimeSheetForm.js
+++ frontend/src/components/TimeSheet/EditTimeSheetForm.js
@@ -24,7 +24,7 @@ const EditTimeSheetForm = ({timesheet}) => {
     if (timeSheetUpdated) {
         return (
             <React.Fragment>
-                <Redirect to={{pathname:'/projects/'+timesheet.project.id, state: { modal: true }}}/>
+                <Redirect to={{pathname: '/projects/' + timesheet.project.id, state: {modal: true}}}/>
             </React.Fragment>
         );
     }
@@ -45,7 +45,7 @@ const EditTimeSheetForm = ({timesheet}) => {
         return {
             "timeUnit": timesheet.timeUnit,
             "defaultIsBillable": timesheet.defaultIsBillable,
-            "expirationDate": moment(timesheet.expirationDate, 'yyyy-MM-dd').utc(),
+            "expirationDate": timesheet.expirationDate ? moment(timesheet.expirationDate, 'YYYY-MM-DD').utc(true) : null,
             "maxDuration": timesheet.maxDuration,
             "durationUnit": timesheet.durationUnit
         }
@@ -54,23 +54,24 @@ const EditTimeSheetForm = ({timesheet}) => {
 
     return (
         <React.Fragment>
-            <Form id="tk_Form" layout="vertical" initialValues={initialValues(timesheet)} onFinish={timeKeeperAPIPut.run} form={form}>
+            <Form id="tk_Form" layout="vertical" initialValues={initialValues(timesheet)}
+                  onFinish={timeKeeperAPIPut.run} form={form}>
 
                 <Form.Item name="timeUnit" label="Time unit" rules={[{required: true}]}>
                     <Radio.Group>
-                        <Radio value="DAY">Day</Radio>
-                        <Radio value="HALFDAY" >Half-day</Radio>
-                        <Radio value="HOURLY" >Hours</Radio>
+                        <Radio.Button value="DAY">Day</Radio.Button>
+                        <Radio.Button value="HALFDAY">Half-day</Radio.Button>
+                        <Radio.Button value="HOURLY">Hours</Radio.Button>
                     </Radio.Group>
                 </Form.Item>
 
-                <Form.Item label="End date" name="expirationDate" rules={[{required: false,},]}>
-                    <DatePicker className="tk_InputDate" />
+                <Form.Item label="End date" name='expirationDate' rules={[{required: false}]}>
+                    <DatePicker className="tk_InputDate"/>
                 </Form.Item>
 
                 <Row gutter={32}>
                     <Col className="gutter-row" span={12}>
-                        <Form.Item label="Number of days" name="maxDuration" rules={[{required: false},]}>
+                        <Form.Item label="Number of days" name="maxDuration" rules={[{required: false}]}>
                             <Input placeholder="TimeSheet's max duration"/>
                         </Form.Item>
                     </Col>
@@ -85,11 +86,11 @@ const EditTimeSheetForm = ({timesheet}) => {
                     </Col>
                 </Row>
 
-                <Form.Item name='durationUnit' noStyle={true} />
+                <Form.Item name='durationUnit' noStyle={true}/>
 
                 <Form.Item>
                     <Space className="tk_JcFe" size="middle" align="center">
-                        <Link id="tk_Btn" className="tk_BtnSecondary" key="cancelLink" to={{state: { modal: true }}}>Cancel</Link>
+                        <Link id="tk_Btn" className="tk_BtnSecondary" key="cancelLink" to={{state: {modal: true}}}>Cancel</Link>
                         <Button id="tk_Btn" className="tk_BtnPrimary" htmlType="submit">Submit</Button>
                     </Space>
                 </Form.Item>
