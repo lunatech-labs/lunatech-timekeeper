b72781e5802a7d59fd32a197b0ca630f3c1e999c
thierry kuy <kuy.thierry@gmail.com>
2020-06-15 12:29:11 +0200

    Time entries enhancement and Proptypes

--- frontend/src/pages/TimeSheet/timeEntriesPage.js
+++ frontend/src/pages/TimeSheet/timeEntriesPage.js
@@ -30,9 +30,9 @@ const TimeEntriesPage = () => {
     return (
       <React.Fragment>
         <Alert title='Server error'
-          message='Failed to load the list of TimeSheets for this user'
-          type='error'
-          description='check that the authenticated User has role [user] on Quarkus'
+               message='Failed to load the list of TimeSheets for this user'
+               type='error'
+               description='check that the authenticated User has role [user] on Quarkus'
         />
       </React.Fragment>
     );
@@ -92,14 +92,10 @@ const TimeEntriesPage = () => {
         dateCellRender={(data) => {
           return (
             <div>
-              {data.map(entry => {
-                if (entry) {
-                  return (
-                    <TimeEntry entry={entry}/>
-                  );
-                } else {
-                  return null;
-                }
+              {data.filter(data => !!data).map(entry => {
+                return (
+                  <TimeEntry key={entry.id} entry={entry}/>
+                );
               })}
             </div>
           );
