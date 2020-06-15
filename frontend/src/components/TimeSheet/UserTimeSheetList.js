8e7cc5223bb43f165920056591b07cb2d1dc61e4
dix <francois.dixmeras@lunatech.fr>
2020-06-15 15:31:38 +0200

    design + conditional formatting for empty values

--- frontend/src/components/TimeSheet/UserTimeSheetList.js
+++ frontend/src/components/TimeSheet/UserTimeSheetList.js
@@ -8,10 +8,23 @@ import TagProjectClient from "../Tag/TagProjectClient";
 import {CalendarOutlined, ClockCircleOutlined} from "@ant-design/icons";
 import moment from 'moment'
 import HistoryOutlined from "@ant-design/icons/lib/icons/HistoryOutlined";
+import './UserTimeSheetList.less';
 
 
 const {Panel} = Collapse;
 const format = (s) => moment(s, 'YYYY-MM-DD').format('YYYY/MM/DD');
+const formatEmpty = (stringTocheck) => {
+    if (stringTocheck)
+        return stringTocheck
+    return "Unlimited"
+};
+
+const defineClassName = (elementToCheck) => {
+    if (elementToCheck)
+        return ''
+    return 'ant-card-meta-description'
+}
+
 
 const UserTimeSheetList = ({timeSheets}) => {
     return (
@@ -23,14 +36,20 @@ const UserTimeSheetList = ({timeSheets}) => {
                     renderItem={timeSheet => {
                         if (!timeSheet.project.publicAccess)
                             return (
-                                <List.Item>
+
+                                <List.Item className='tk_TimeSheetList'>
+                                    <div>
                                     <Avatar src={logo} shape={'square'} size="large"/>
-                                    <div>{timeSheet.project.name}</div>
+                                    <p>{timeSheet.project.name}</p>
                                     <TagProjectClient client={timeSheet.project.client}/>
-                                    <p><ClockCircleOutlined/> TimeUnit: {timeSheet.timeUnit}</p>
-                                    <p><CalendarOutlined/> Number of days: {timeSheet.maxDuration ? timeSheet.maxDuration : "Unlimited"}</p>
-                                    <p><CalendarOutlined/> End date: {timeSheet.expirationDate ? format(timeSheet.expirationDate) : 'Unlimited'}</p>
-                                    <p><HistoryOutlined/>Days left : {timeSheet.leftOver ? timeSheet.leftOver : 'Unlimited'}</p>
+                                    </div>
+                                    <div>
+
+                                    <p className={defineClassName(timeSheet.timeUnit)}><ClockCircleOutlined/> TimeUnit: {timeSheet.timeUnit}</p>
+                                    <p className={defineClassName(timeSheet.maxDuration)}><CalendarOutlined/> Number of days: {formatEmpty(timeSheet.maxDuration)}</p>
+                                    <p className={defineClassName(timeSheet.expirationDate)}><CalendarOutlined/> End date: {timeSheet.expirationDate ? format(timeSheet.expirationDate) : 'Unlimited'}</p>
+                                    <p className={defineClassName(timeSheet.leftOver)}><HistoryOutlined/>Days left : {formatEmpty(timeSheet.leftOver)}</p>
+                                    </div>
                                 </List.Item>
                             )
                     }}
