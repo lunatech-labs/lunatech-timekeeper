01a39183162f0ae23e4ea742eb7dee4a6e160056
tkuy <45755667+tkuy@users.noreply.github.com>
2020-06-15 09:40:14 +0200

    Don't show client side bar button to user (#175)

--- frontend/src/components/MenuSidebar/MenuSidebar.js
+++ frontend/src/components/MenuSidebar/MenuSidebar.js
@@ -1,53 +1,56 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Link, withRouter} from 'react-router-dom';
 import {Menu} from 'antd';
 import './MenuSidebar.less';
 import PropTypes from 'prop-types';
 
-import {UserOutlined, DesktopOutlined, PieChartOutlined} from '@ant-design/icons';
+import {DesktopOutlined, PieChartOutlined, UserOutlined} from '@ant-design/icons';
 import FolderOpenOutlined from '@ant-design/icons/lib/icons/FolderOpenOutlined';
 import ClockCircleOutlined from '@ant-design/icons/lib/icons/ClockCircleOutlined';
+import {useKeycloak} from "@react-keycloak/web";
 
-class MenuSidebar extends Component {
-  render() {
-    const {location} = this.props;
-    const splitPathname = location.pathname.split('/').filter(i=>i);
-    const selectedKeys = splitPathname.length === 0 ? [] : ['/' + splitPathname[0]];
-    return (
-      <Menu id="tk_Menu" defaultSelectedKeys={['/home']} mode="inline" selectedKeys={selectedKeys} theme="dark">
-        <Menu.Item className="tk_MenuItem" key="/home">
-          <Link to="/home">
-            <PieChartOutlined/>
-            <span>Home</span>
-          </Link>
-        </Menu.Item>
-        <Menu.Item className="tk_MenuItem" key="/clients">
-          <Link to="/clients">
-            <DesktopOutlined/>
-            <span>Clients</span>
-          </Link>
-        </Menu.Item>
-        <Menu.Item className="tk_MenuItem" key="/users">
-          <Link to="/users">
-            <UserOutlined/>
-            <span>Users</span>
-          </Link>
-        </Menu.Item>
-        <Menu.Item className="tk_MenuItem" key="/projects">
-          <Link to="/projects">
-            <FolderOpenOutlined />
-            <span>Projects</span>
-          </Link>
-        </Menu.Item>
-        <Menu.Item className="tk_MenuItem" key="/time_entries">
-          <Link to="/time_entries">
-            <ClockCircleOutlined />
-            <span>Time entries</span>
-          </Link>
-        </Menu.Item>
-      </Menu>
-    );
-  }
+const MenuSidebar = (props) => {
+  const [keycloak] = useKeycloak();
+
+  const isAdmin = keycloak.hasRealmRole("admin");
+  const {location} = props;
+  const splitPathname = location.pathname.split('/').filter(i => i);
+  const selectedKeys = splitPathname.length === 0 ? [] : ['/' + splitPathname[0]];
+
+  return (
+    <Menu id="tk_Menu" defaultSelectedKeys={['/home']} mode="inline" selectedKeys={selectedKeys} theme="dark">
+      <Menu.Item className="tk_MenuItem" key="/home">
+        <Link to="/home">
+          <PieChartOutlined/>
+          <span>Home</span>
+        </Link>
+      </Menu.Item>
+      {isAdmin && <Menu.Item className="tk_MenuItem" key="/clients">
+        <Link to="/clients">
+          <DesktopOutlined/>
+          <span>Clients</span>
+        </Link>
+      </Menu.Item>}
+      <Menu.Item className="tk_MenuItem" key="/users">
+        <Link to="/users">
+          <UserOutlined/>
+          <span>Users</span>
+        </Link>
+      </Menu.Item>
+      <Menu.Item className="tk_MenuItem" key="/projects">
+        <Link to="/projects">
+          <FolderOpenOutlined/>
+          <span>Projects</span>
+        </Link>
+      </Menu.Item>
+      <Menu.Item className="tk_MenuItem" key="/time_entries">
+        <Link to="/time_entries">
+          <ClockCircleOutlined/>
+          <span>Time entries</span>
+        </Link>
+      </Menu.Item>
+    </Menu>
+  );
 }
 
 MenuSidebar.propTypes = {
