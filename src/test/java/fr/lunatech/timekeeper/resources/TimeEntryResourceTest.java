6650e3a249978caa1ecca5fe78abf0106228f1e4
dix <francois.dixmeras@lunatech.fr>
2020-06-16 18:38:02 +0200

    after merge fix: add new field in test

--- src/test/java/fr/lunatech/timekeeper/resources/TimeEntryResourceTest.java
+++ src/test/java/fr/lunatech/timekeeper/resources/TimeEntryResourceTest.java
@@ -193,7 +193,7 @@ class TimeEntryResourceTest {
         final ProjectResponse project = create(new ProjectRequest("Some Project", true, "some description", client.getId(), true, newUsers), adminToken);
 
         // ID is 2 since Jimmy timesheet is created after Sam's one - Sam is added before Jimmy to the Project
-        final var expectedTimeSheetJimmy = new TimeSheetResponse(2L, project, jimmy, TimeUnit.HOURLY, true, null, null, TimeUnit.HOURLY.toString(), Collections.emptyList());
+        final var expectedTimeSheetJimmy = new TimeSheetResponse(2L, project, jimmy, TimeUnit.HOURLY, true, null, null, TimeUnit.DAY.toString(), Collections.emptyList(),null);
 
         getValidation(TimeSheetPerProjectPerUserDef.uriWithMultiId(project.getId(), jimmy.getId()), adminToken, OK).body(is(timeKeeperTestUtils.toJson(expectedTimeSheetJimmy)));
         LocalDate date = LocalDate.of(2020, 1, 1);
