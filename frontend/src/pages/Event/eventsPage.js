ce16b4e8f45b3619ff48ca39bc0082013c37dfff
geoffroy bouet <geoffroy.bouet@lunatech.fr>
2020-06-22 17:00:15 +0200

    TK-405_Wrong_order_and_icon_main_menu

---
+++ frontend/src/pages/Event/eventsPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainPage from '../MainPage/MainPage';
+
+
+const EventsPage = () => {
+  return (
+    <MainPage title="Events">
+
+    </MainPage>
+  );
+};
+
+export default EventsPage;
+
