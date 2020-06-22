80cf103c10bfbe267f9cbd6fc8a6b44a3b5a02b2
geoffroy bouet <geoffroy.bouet@lunatech.fr>
2020-06-22 10:29:51 +0200

    antd_icons_import

--- frontend/src/components/TimeEntry/ShowTimeEntry.js
+++ frontend/src/components/TimeEntry/ShowTimeEntry.js
@@ -3,9 +3,7 @@ import PropTypes from 'prop-types';
 import './ShowTimeEntry.less';
 import {Button} from 'antd';
 import Tooltip from 'antd/lib/tooltip';
-import EditOutlined from "@ant-design/icons/es/icons/EditOutlined";
-import CopyOutlined from "@ant-design/icons/es/icons/CopyOutlined";
-import DeleteOutlined from "@ant-design/icons/es/icons/DeleteOutlined";
+import {FieldTimeOutlined, DeleteOutlined, CopyOutlined, EditOutlined} from '@ant-design/icons';
 
 const moment = require('moment');
 
@@ -27,7 +25,7 @@ const ShowTimeEntry = ({entry}) => {
         <div className="tk_TaskInfoMiddle">
           <div>
             <p>{entry.project ? entry.project.name : ''}</p>
-            <p>{date.format('hh:mm')}</p>
+            <p><FieldTimeOutlined />{date.format('hh:mm')}</p>
           </div>
           <div>
             <Tooltip title="Edit" key="edit">
