93c39fb0fc3344834bb6546e631e0be192cff4d5
geoffroy bouet <geoffroy.bouet@lunatech.fr>
2020-06-30 15:32:14 +0200

    design_dropdown_eventmember

--- frontend/src/components/Events/EventMemberTag.js
+++ frontend/src/components/Events/EventMemberTag.js
@@ -3,49 +3,47 @@ import './EventMemberTag.less';
 import PropTypes from 'prop-types';
 import {Alert, Avatar, Spin} from 'antd';
 import CardXs from '../Card/CardXs';
-import {useTimeKeeperAPI} from "../../utils/services";
+import {useTimeKeeperAPI} from '../../utils/services';
 
 const EventMemberTag = ({ member }) => {
-    const usersResponse = useTimeKeeperAPI('/api/users');
+  const usersResponse = useTimeKeeperAPI('/api/users');
 
-    if (usersResponse.loading) {
-        return (
-            <React.Fragment>
-                <Spin size="large">
-                    <p>Loading list of projects</p>
-                </Spin>
-            </React.Fragment>
-        );
-    }
-
-    if (usersResponse.error) {
-        return (
-            <React.Fragment>
-                <Alert title='Server error'
-                       message='Failed to load the list of projects'
-                       type='error'
-                       description='Unable to fetch the list of Projects from the server'
-                />
-            </React.Fragment>
-        );
-    }
-
-    const user = usersResponse.data.filter(user => user.id === member.userId);
+  if (usersResponse.loading) {
     return (
-        <CardXs>
-            <div>
-                <Avatar src={user[0].picture}/>
-                <p>{user[0].name}</p>
-            </div>
-        </CardXs>
+      <React.Fragment>
+        <Spin size="large">
+          <p>Loading list of projects</p>
+        </Spin>
+      </React.Fragment>
     );
+  }
+
+  if (usersResponse.error) {
+    return (
+      <React.Fragment>
+        <Alert title='Server error'
+          message='Failed to load the list of projects'
+          type='error'
+          description='Unable to fetch the list of Projects from the server'
+        />
+      </React.Fragment>
+    );
+  }
+
+  const user = usersResponse.data.filter(user => user.id === member.userId);
+  return (
+    <div className="tk_">
+      <Avatar src={user[0].picture}/>
+      <p>{user[0].name}</p>
+    </div>
+  );
 };
 
 EventMemberTag.propTypes = {
-    member: PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        picture: PropTypes.string
-    })
+  member: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    picture: PropTypes.string
+  })
 };
 
 export default EventMemberTag;
