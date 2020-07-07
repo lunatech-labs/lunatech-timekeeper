a6b299b2e463a0b9dc1c7240cacbcae4153c0c28
thierry kuy <kuy.thierry@gmail.com>
2020-07-07 10:01:31 +0200

    Remove roles

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -33,7 +33,7 @@ export const AppRouter = () => {
         <PrivateRoute exact path="/home"   component={HomePage} />
         <PrivateRoute path="/users"        component={UsersPage} roles={['admin']} />
         <PrivateRoute path="/projects/new" component={NewProjectPage} roles={['admin']} />
-        <PrivateRoute path="/projects/:id/edit" component={EditProjectsPage} roles={['admin','user']} />
+        <PrivateRoute path="/projects/:id/edit" component={EditProjectsPage} />
         <PrivateRoute path="/projects/:id" component={DetailProjectPage} />
         <PrivateRoute path="/projects"     component={ProjectsPage} />
         <PrivateRoute path="/clients/new"  component={NewClientPage} roles={['admin']} />
