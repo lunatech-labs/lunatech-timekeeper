e429408c21f422850bdde8abb5234e107a45c40d
Guillaume <guillaume.davy@lunatech.fr>
2020-07-20 11:07:00 +0200

    Edit event

--- frontend/src/pages/Event/EditEventTemplatePage.js
+++ frontend/src/pages/Event/EditEventTemplatePage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import MainPage from '../MainPage/MainPage';
+import EditEventTemplateForm from '../../components/Events/EditEventTemplateForm';
 
 
 const EditEventTemplatePage = () => {
     return (
         <MainPage title="Edit an event">
+            <EditEventTemplateForm />
         </MainPage>
     );
 };
