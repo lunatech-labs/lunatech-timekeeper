63a2ccafe84d632923d3df169943f2070b2cd6f2
Guillaume <guillaume.davy@lunatech.fr>
2020-07-20 10:10:25 +0200

    Route, EditPage, Breadcrumbs

---
+++ frontend/src/pages/Event/EditEventTemplatePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainPage from '../MainPage/MainPage';
+
+
+const EditEventTemplatePage = () => {
+    return (
+        <MainPage title="Edit an event">
+        </MainPage>
+    );
+};
+
+export default EditEventTemplatePage;
