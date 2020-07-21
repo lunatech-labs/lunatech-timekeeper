030a372ef85484e8a0371fc99b26830ec2fbf54f
Nicolas Martignole <nicolas@martignole.net>
2020-07-21 13:51:49 +0200

    Copyright

--- src/test/java/fr/lunatech/timekeeper/resources/TimeEntryResourceTest.java
+++ src/test/java/fr/lunatech/timekeeper/resources/TimeEntryResourceTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Lunatech Labs
+ * Copyright 2020 Lunatech S.A.S
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
