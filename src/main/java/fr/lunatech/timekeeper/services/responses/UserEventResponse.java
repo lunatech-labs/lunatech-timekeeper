1f0e2db46fb84faa8c662001a52c8d7ac3d1dc02
Guillaume <guillaume.davy@lunatech.fr>
2020-08-07 11:09:49 +0200

    backend

--- src/main/java/fr/lunatech/timekeeper/services/responses/UserEventResponse.java
+++ src/main/java/fr/lunatech/timekeeper/services/responses/UserEventResponse.java
@@ -20,11 +20,17 @@ import fr.lunatech.timekeeper.models.time.UserEvent;
 import fr.lunatech.timekeeper.timeutils.TimeKeeperDateUtils;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserEventResponse {
     private Long id;
     private String name;
     private String description;
+    private List<EventUserDayResponse> eventUserDaysResponse;
     private String startDateTime;
     private String endDateTime;
     private String date;
@@ -35,12 +41,41 @@ public class UserEventResponse {
 
     }
 
+    /**
+     * bind UserEvent to UserEventResponse with an eventUserDayResponse
+     * For the eventUserDayResponse, it uses a sequential ordered stream of dates. The returned stream starts from this date
+     * (inclusive) and goes to {@code endExclusive} (exclusive) by an incremental step of 1 day. Because it goes to the exclusive end date,
+     * this uses the endTimeDate + 1.
+     * @param event
+     * @return UserEventResponse
+     */
     public static UserEventResponse bind(UserEvent event) {
         UserEventResponse response = new UserEventResponse();
         response.id = event.id;
         response.name = event.name;
         response.description = event.description;
 
+        if(event.startDateTime != null && event.endDateTime != null){
+            List<LocalDate> dates = event.startDateTime.toLocalDate().datesUntil(event.endDateTime.toLocalDate().plusDays(1))
+                    .collect(Collectors.toList());
+            if(dates.size() > 1){
+                response.eventUserDaysResponse = dates.stream()
+                        .map(date -> {
+                            if(date.isEqual(event.startDateTime.toLocalDate())){
+                                return new EventUserDayResponse(event.name,event.description,TimeKeeperDateUtils.formatToString(date.atTime(event.startDateTime.getHour(), event.startDateTime.getMinute())), TimeKeeperDateUtils.formatToString(date.atTime(17, 0)),TimeKeeperDateUtils.formatToString(date));
+                            }else if(date.isEqual(event.endDateTime.toLocalDate())){
+                                return new EventUserDayResponse(event.name,event.description,TimeKeeperDateUtils.formatToString(date.atTime(9, 0)), TimeKeeperDateUtils.formatToString(date.atTime(event.endDateTime.getHour(), event.endDateTime.getMinute())),TimeKeeperDateUtils.formatToString(date));
+                            } else {
+                                return new EventUserDayResponse(event.name,event.description,TimeKeeperDateUtils.formatToString(date.atTime(9, 0)), TimeKeeperDateUtils.formatToString(date.atTime(17, 0)),TimeKeeperDateUtils.formatToString(date));
+                            }
+                        })
+                        .collect(Collectors.toList());
+            } else {
+                response.eventUserDaysResponse = dates.stream()
+                        .map(date -> new EventUserDayResponse(event.name,event.description,TimeKeeperDateUtils.formatToString(date.atTime(event.startDateTime.getHour(), event.startDateTime.getMinute())), TimeKeeperDateUtils.formatToString(date.atTime(event.endDateTime.getHour(), event.endDateTime.getMinute())),TimeKeeperDateUtils.formatToString(date)))
+                        .collect(Collectors.toList());
+            }
+        }
         if (event.startDateTime != null) {
             response.startDateTime = TimeKeeperDateUtils.formatToString(event.startDateTime);
         }
@@ -75,6 +110,10 @@ public class UserEventResponse {
         return description;
     }
 
+    public List<EventUserDayResponse> getEventUserDaysResponse() {
+        return eventUserDaysResponse;
+    }
+
     public String getStartDateTime() {
         return startDateTime;
     }
@@ -98,12 +137,49 @@ public class UserEventResponse {
        return duration;
     }
 
+    public static class EventUserDayResponse {
+        private String name;
+        private String description;
+        private String startDateTime;
+        private String endDateTime;
+        private String date;
+
+        public EventUserDayResponse(String name, String description, String startDateTime, String endDateTime, String date) {
+            this.name = name;
+            this.description = description;
+            this.startDateTime = startDateTime;
+            this.endDateTime = endDateTime;
+            this.date = date;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getStartDateTime() {
+            return startDateTime;
+        }
+
+        public String getEndDateTime() {
+            return endDateTime;
+        }
+
+        public String getDate() {
+            return date;
+        }
+    }
+
     @Override
     public String toString() {
         return "UserEventResponse{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", description='" + description + '\'' +
+                ", eventUserDaysResponse=" + eventUserDaysResponse +
                 ", startDateTime='" + startDateTime + '\'' +
                 ", endDateTime='" + endDateTime + '\'' +
                 ", date='" + date + '\'' +
