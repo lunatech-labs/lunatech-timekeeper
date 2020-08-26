4397f693afac7725b6563107c976996ee0c34e9b
Guillaume <guillaume.davy@lunatech.fr>
2020-08-26 15:16:21 +0200

    sonarcloud changes

--- src/main/java/fr/lunatech/timekeeper/services/UserEventService.java
+++ src/main/java/fr/lunatech/timekeeper/services/UserEventService.java
@@ -10,6 +10,7 @@ import javax.enterprise.context.ApplicationScoped;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 
 @ApplicationScoped
@@ -18,28 +19,26 @@ public class UserEventService {
     private static final Logger logger = LoggerFactory.getLogger(EventTemplateService.class);
 
     public List<UserEventResponse> getEventsByUserForWeekNumber(Long ownerId, Integer weekNumber, Integer year){
-        logger.debug("getEventsForUser {}", ownerId);
         return getEventsByUser(ownerId,weekNumber,year,TimeKeeperDateUtils::getWeekNumberFromDate);
     }
 
     public List<UserEventResponse> getEventsByUserForMonthNumber(Long ownerId, Integer monthNumber, Integer year){
-        logger.debug("getEventsForUser {}", ownerId);
         return getEventsByUser(ownerId,monthNumber,year,TimeKeeperDateUtils::getMonthNumberFromDate);
     }
 
-    protected List<UserEventResponse> getEventsByUser(Long ownerId, Integer monthNumber, Integer year, Function<LocalDate, Integer> getWeekOfMonthNumberFromDate){
+    protected List<UserEventResponse> getEventsByUser(Long ownerId, Integer monthNumber, Integer year, ToIntFunction<LocalDate> getWeekOfMonthNumberFromDate){
         logger.debug("getEventsForUser {}", ownerId);
         return UserEvent.<UserEvent>stream("owner_id=?1", ownerId)
                 .filter(userEvent -> isUserEventInWeekOrMonthNumber(userEvent, monthNumber, year, getWeekOfMonthNumberFromDate))
                 .map(UserEventResponse::bind)
                 .collect(Collectors.toList());
     }
-    protected boolean isUserEventInWeekOrMonthNumber(UserEvent userEvent, Integer weekOrMonthNumber, Integer year, Function<LocalDate, Integer> getWeekOfMonthNumberFromDate){
-        var startDateTimeNumber = getWeekOfMonthNumberFromDate.apply(userEvent.startDateTime.toLocalDate());
-        var endDateTimeNumber = getWeekOfMonthNumberFromDate.apply(userEvent.endDateTime.toLocalDate());
+    protected boolean isUserEventInWeekOrMonthNumber(UserEvent userEvent, Integer weekOrMonthNumber, Integer year, ToIntFunction<LocalDate> getWeekOfMonthNumberFromDate){
+        var startDateTimeNumber = getWeekOfMonthNumberFromDate.applyAsInt(userEvent.startDateTime.toLocalDate());
+        var endDateTimeNumber = getWeekOfMonthNumberFromDate.applyAsInt(userEvent.endDateTime.toLocalDate());
         return (validateYear(userEvent, year))
                 && ((startDateTimeNumber <= weekOrMonthNumber && endDateTimeNumber >= weekOrMonthNumber)
-                || (startDateTimeNumber.equals(weekOrMonthNumber) || endDateTimeNumber.equals(weekOrMonthNumber)));
+                || (startDateTimeNumber == weekOrMonthNumber || endDateTimeNumber == weekOrMonthNumber));
     }
 
     protected boolean validateYear(UserEvent userEvent, Integer year){
