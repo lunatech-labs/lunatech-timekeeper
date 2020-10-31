2eb90ca47e4225b91cece501e4fa95c10965f217
Aniket <aniket.kakde@lunatech.fr>
2020-10-31 23:39:13 +0100

    Add some WeekCalendar related component

---
+++ frontend/src/components/TimeSheet/WeekCalendar/TimeEntriesForADayForAWeekCalendar.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2020 Lunatech S.A.S
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import TimeEntry from '../../TimeEntry/TimeEntry';
+import moment from 'moment';
+
+const TimeEntriesForADayForAWeekCalendar = (props) => {
+    const {timeEntries, onClickEntryCard} = props;
+    console.log("timeEntries: " + timeEntries.date);
+    return (timeEntries && timeEntries.timeEntriesForADay && timeEntries.timeEntriesForADay.data) ?
+        timeEntries.timeEntriesForADay.data.map(entry => {
+            return (
+                <TimeEntry key={entry.id} entry={entry}
+                           onClick={mouseEvent => onClickEntryCard(mouseEvent, moment().utc(), entry.id)}
+                />
+            );
+        }) : <></>;
+};
+export default TimeEntriesForADayForAWeekCalendar;
---
+++ frontend/src/components/TimeSheet/WeekCalendar/WeekDayCard.js
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2020 Lunatech S.A.S
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import './WeekCalendar.less';
+import moment from 'moment';
+import DisplayTopRightCorner from '../MonthCalendar/DisplayTopRightCorner';
+import TimeEntriesForADayForAWeekCalendar from './TimeEntriesForADayForAWeekCalendar';
+import UserEventsForADayForWeekCalendar from './UserEventsForADayForWeekCalendar';
+
+const WeekDayCard = (props) => {
+    const { onClickCard, onClickButton, onClickEntryCard, dateAsMoment, timeEntries, eventEntries, isItPublicHoliday, hoursCompleted, isDayDisabled, isDayWithoutAnyEntries} = props;
+
+    const applyClass2 = (isDayWithoutAnyEntries && !isDayDisabled) ? 'tk_CardWeekCalendar_Body_With_Warn' : isDayDisabled ? 'tk_CardWeekCalendar_Body' : '';
+    const applyClass = 'tk_CardWeekCalendar_Body';
+    const isToday = (day) => {
+        return moment().isSame(day, 'day');
+    };
+
+    return (
+        <div className="tk_WeekCalendar_Day" key={`WeekCalendarDay-${timeEntries.date.toString()}`}>
+            <p>{timeEntries.date.format('ddd')}</p>
+            <div className="tk_CardWeekCalendar">
+                <div className="tk_CardWeekCalendar_Head">
+                    <p className={isToday(moment(timeEntries.date)) ? 'tk_CurrentDay' : ''}>{timeEntries.date.format('Do')}</p>
+                    <DisplayTopRightCorner
+                        isPublicHoliday={isItPublicHoliday}
+                        hoursCompleted={hoursCompleted}
+                        dateAsMoment={timeEntries.date}
+                        onClickPlusButton={onClickButton}
+                        isDisabled={isDayDisabled}
+                    />
+                </div>
+                <div onClick={(e) => onClickCard && onClickCard(e, dateAsMoment)} className={applyClass}>
+                    <UserEventsForADayForWeekCalendar eventEntries={eventEntries}/>
+                    <TimeEntriesForADayForAWeekCalendar
+                        timeEntries={timeEntries}
+                        onClickEntryCard={onClickEntryCard}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+export default WeekDayCard;
